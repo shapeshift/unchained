ad66dd789f110bff4222b7acb11e64c4173a7b07
highlander <bithighlander@gmail.com>
2021-09-07 12:49:54 -0600

    init bitcoin infra

---
+++ coinstacks/bitcoin/pulumi/index.ts
@@ -0,0 +1,65 @@
+import { config as getEnv, parse } from 'dotenv'
+import { existsSync, readFileSync } from 'fs'
+import { join } from 'path'
+import * as k8s from '@pulumi/kubernetes'
+import { all } from '@pulumi/pulumi'
+import { deployApi, deployMongo, deployIngester } from '@shapeshiftoss/common-pulumi'
+import { deployIndexer } from '@shapeshiftoss/blockbook-pulumi'
+import { getConfig } from './config'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Outputs = Record<string, any>
+
+//https://www.pulumi.com/docs/intro/languages/javascript/#entrypoint
+export = async (): Promise<Outputs> => {
+  const app = 'unchained'
+  const namespace = 'unchained'
+  const asset = 'bitcoin'
+  const outputs: Outputs = {}
+
+  const { kubeconfig, config } = await getConfig()
+  const { cluster } = config
+
+  let provider: k8s.Provider
+  if (config.isLocal) {
+    provider = new k8s.Provider('kube-provider', { cluster, context: cluster })
+  } else {
+    provider = new k8s.Provider('kube-provider', { kubeconfig })
+  }
+
+  if (existsSync('../.env')) {
+    getEnv({ path: join(__dirname, '../.env') })
+  } else if (config.isLocal) {
+    throw new Error(
+      'you must run `cp sample.env .env` from the bitcoin coinstack directory and fill out any empty values.'
+    )
+  }
+
+  const missingKeys: Array<string> = []
+  const stringData = Object.keys(parse(readFileSync('../sample.env'))).reduce((prev, key) => {
+    const value = process.env[key]
+
+    if (!value) {
+      missingKeys.push(key)
+      return prev
+    }
+
+    return { ...prev, [key]: value }
+  }, {})
+
+  if (missingKeys.length) {
+    throw new Error(`Missing the following required environment variables: ${missingKeys.join(', ')}`)
+  }
+
+  new k8s.core.v1.Secret(asset, { metadata: { name: asset, namespace }, stringData }, { provider })
+
+  const mongo = await deployMongo(app, asset, provider, namespace, config.mongo)
+
+  const deps = all([mongo]).apply(([mongoResources]) => mongoResources)
+
+  await deployIndexer(app, asset, provider, namespace, config)
+  await deployIngester(app, asset, provider, namespace, config, deps)
+  await deployApi(app, asset, provider, namespace, config, deps)
+
+  return outputs
+}
