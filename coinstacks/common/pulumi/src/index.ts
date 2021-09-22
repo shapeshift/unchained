b27b7bdb3822bdfedf235de29cf468d52da6416d
parker <hewittparker@gmail.com>
2021-09-22 13:29:25 -0600

    make additionalEnvironments optional in BaseConfig

--- coinstacks/common/pulumi/src/index.ts
+++ coinstacks/common/pulumi/src/index.ts
@@ -24,7 +24,7 @@ export interface BaseConfig {
   dockerhub?: Dockerhub
   cluster: Cluster
   isLocal: boolean
-  additionalEnvironments: string[]
+  additionalEnvironments?: string[]
   /**
    * Creates ingress for public dns
    *
