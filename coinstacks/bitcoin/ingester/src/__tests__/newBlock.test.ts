206cd6c5700fbcd066609e047ecb1a2c77fa05e9
mvpratt <24981541+mvpratt@users.noreply.github.com>
2021-09-22 13:00:51 -0400

    Add Bitcoin ingester (#29)
    New blocks:
    * subscribe to new blocks from the node and put in the block queue
    * parse transactions from block and put in the txid queue
    * parse addresses from each transaction and check the registry.
    
    Mempool:
    * subscribe to new transactions from mempool and put in the txid queue

---
+++ coinstacks/bitcoin/ingester/src/__tests__/newBlock.test.ts
@@ -0,0 +1,112 @@
+import { Worker } from '@shapeshiftoss/common-ingester'
+import { handleReorg } from '../workers/newBlock'
+import { db, node } from './__mocks__/reorg'
+
+const sendMessageMock = jest.fn()
+jest.mock('@shapeshiftoss/common-ingester', () => ({
+  Worker: {
+    init: () => ({
+      queue: {
+        prefetch: jest.fn(),
+        activateConsumer: jest.fn(),
+      },
+      sendMessage: () => sendMessageMock(),
+    }),
+  },
+  Message: jest.fn(),
+}))
+
+jest.mock('axios', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post: (_: string, data: any) => {
+    const block = Object.values(node).find((v) => data.params[0] === v.hash)
+    return { data: { result: block } }
+  },
+}))
+
+const orphanMock = jest.fn()
+jest.mock('@shapeshiftoss/common-mongo', () => ({
+  BlockService: jest.fn().mockImplementation(() => ({
+    getByHash: (hash: string) => Object.values(db).find((v) => hash === v.hash),
+    orphan: () => orphanMock(),
+  })),
+}))
+
+describe('newBlockWorker', () => {
+  beforeEach(() => {
+    orphanMock.mockClear()
+    sendMessageMock.mockClear()
+  })
+
+  describe('handleReorg', () => {
+    it('should handle a reorg if there are orphaned blocks in db', async () => {
+      const worker = await Worker.init({})
+
+      // Note: blocks 700972, 700971, 700970 in the db have incorrect hashes in order to trigger a reorg
+      const dbLatestBlock = db[700972]
+      const nodeBlock = node[700973]
+
+      const expected = {
+        height: 700970,
+        dbBlock: {
+          hash: node[700970].hash,
+          height: Number(node[700970].height),
+          prevHash: node[700970].previousblockhash,
+        },
+        nodeBlock: node[700970],
+      }
+
+      const actual = await handleReorg(worker, dbLatestBlock, nodeBlock)
+
+      expect(orphanMock).toHaveBeenCalledTimes(3) // 3,
+      expect(sendMessageMock).toHaveBeenCalledTimes(3) // 3
+      expect(actual).toEqual(expected)
+    })
+
+    it('should use node height if no blocks in db', async () => {
+      const worker = await Worker.init({})
+
+      const dbLatestBlock = undefined
+      const nodeBlock = node[700971]
+
+      const expected = {
+        height: 700971,
+        dbBlock: {
+          hash: node[700971].hash,
+          height: Number(node[700971].height),
+          prevHash: node[700971].previousblockhash,
+        },
+        nodeBlock: node[700971],
+      }
+
+      const actual = await handleReorg(worker, dbLatestBlock, nodeBlock)
+
+      expect(orphanMock).toHaveBeenCalledTimes(0)
+      expect(sendMessageMock).toHaveBeenCalledTimes(0)
+      expect(actual).toEqual(expected)
+    })
+
+    it('should use node height if no reorg detected', async () => {
+      const worker = await Worker.init({})
+
+      const dbLatestBlock = db[700969]
+      const nodeBlock = node[700970]
+
+      const expected = {
+        height: 700970,
+        dbBlock: {
+          hash: node[700970].hash,
+          height: Number(node[700970].height),
+          prevHash: node[700970].previousblockhash,
+        },
+        nodeBlock: node[700970],
+      }
+
+      const actual = await handleReorg(worker, dbLatestBlock, nodeBlock)
+
+      expect(orphanMock).toHaveBeenCalledTimes(0)
+      expect(sendMessageMock).toHaveBeenCalledTimes(0)
+      expect(actual).toEqual(expected)
+    })
+  })
+})
---
+++ coinstacks/bitcoin/ingester/src/workers/block.ts
@@ -0,0 +1,23 @@
+import { Worker, Message } from '@shapeshiftoss/common-ingester'
+import { logger } from '@shapeshiftoss/logger'
+import { BTCBlock } from '../types'
+
+const onMessage = (worker: Worker) => (message: Message) => {
+  const block: BTCBlock = message.getContent()
+  logger.debug(`block: (${Number(block.height)}) ${block.hash}`)
+
+  block.tx.forEach((txid) => worker.sendMessage(new Message(txid), 'txid'))
+  worker.ackMessage(message)
+}
+
+const main = async () => {
+  const worker = await Worker.init({
+    queueName: `queue.bitcoin.block`,
+    exchangeName: `exchange.bitcoin.txid`,
+  })
+
+  worker.queue?.prefetch(1)
+  worker.queue?.activateConsumer(onMessage(worker), { noAck: false })
+}
+
+main()
