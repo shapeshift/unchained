34d9c90ca1d66bd07f5767e551a86d5d4fcb2071
kaladinlight <kaladin.light@gmail.com>
2021-10-11 16:39:40 -0600

    implement heartbeat

--- coinstacks/ethereum/api/src/websocket.ts
+++ coinstacks/ethereum/api/src/websocket.ts
@@ -4,17 +4,22 @@ import { SequencedETHParseTx } from '@shapeshiftoss/ethereum-ingester'
 
 export { SequencedETHParseTx, ErrorResponse, RequestPayload, TxsTopicData }
 
+export interface Connection {
+  ws: WebSocket
+  pingTimeout?: NodeJS.Timeout
+}
+
 export class Client {
   private readonly url: string
-  private readonly websockets: Record<Topics, WebSocket | undefined>
+  private readonly connections: Record<Topics, Connection | undefined>
   private readonly sequencedData: Record<Topics, Array<boolean> | undefined>
   private readonly opts?: WebSocket.ClientOptions
 
   constructor(url: string, opts?: WebSocket.ClientOptions) {
     this.url = url
-    this.opts = opts
+    this.opts = { ...opts, sessionTimeout: 10000 }
 
-    this.websockets = {
+    this.connections = {
       txs: undefined,
     }
 
@@ -23,21 +28,35 @@ export class Client {
     }
   }
 
+  private heartbeat(topic: Topics): void {
+    const connection = this.connections[topic]
+    if (!connection) return
+
+    connection.pingTimeout && clearTimeout(connection.pingTimeout)
+    connection.pingTimeout = setTimeout(() => connection?.ws.terminate(), 10000 + 1000)
+  }
+
+  private onOpen(topic: Topics, resolve: (value: unknown) => void): void {
+    this.heartbeat(topic)
+    resolve(true)
+  }
+
   // TODO: add onError callback for any error cases
   // TODO: add batch key
   async subscribeTxs(
     data: TxsTopicData,
     onMessage: (message: SequencedETHParseTx | ErrorResponse) => void
   ): Promise<void> {
-    if (this.websockets.txs) return
+    if (this.connections.txs) return
 
     const ws = new WebSocket(this.url, this.opts)
 
-    ws.onerror = (event) => console.log('error', event)
+    this.connections.txs = { ws }
 
-    this.websockets.txs = ws
+    await new Promise((resolve) => (ws.onopen = () => this.onOpen('txs', resolve)))
 
-    await new Promise((resolve) => (ws.onopen = () => resolve(true)))
+    ws.onclose = () => this.connections.txs?.pingTimeout && clearTimeout(this.connections.txs.pingTimeout)
+    ws.on('ping', () => this.heartbeat('txs'))
 
     const payload: RequestPayload = { method: 'subscribe', topic: 'txs', data }
 
@@ -67,13 +86,13 @@ export class Client {
 
   unsubscribeTxs(): void {
     this.sequencedData.txs = undefined
-    this.websockets.txs?.send(
+    this.connections.txs?.ws.send(
       JSON.stringify({ method: 'unsubscribe', topic: 'txs', data: undefined } as RequestPayload)
     )
   }
 
   close(topic: Topics): void {
     this.sequencedData[topic] = undefined
-    this.websockets[topic]?.close()
+    this.connections[topic]?.ws.close()
   }
 }
