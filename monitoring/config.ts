a554d90580be69fa5ca72847d12c6bed5fd5f4b1
samortaylor <samortaylor@gmail.com>
2021-11-19 09:08:16 -0700

    Monitoring stack (#154)
    * init monitoring stack
    
    * consult with Kevin
    
    * fix circleci config
    
    * yaml hell
    
    * use circleci config validator
    
    * lint
    
    * add docs
    
    * don't use logging facilities in cluster launcher
    
    * leverage new version to include prometheus scrape annotations on cluster-autoscaler and traefik
    
    * Ensure we have enough storage for verbose logs

---
+++ monitoring/config.ts
@@ -0,0 +1,51 @@
+import * as pulumi from '@pulumi/pulumi'
+
+export interface MonitoringConfig {
+  stack: string
+  environment: string
+  githubOauthID: string
+  githubOauthSecret: string
+  githubOrg: string
+}
+
+export interface LoopConfig {
+  kubeconfig: string
+  config: MonitoringConfig
+  namespace: string
+}
+export const getConfig = async (): Promise<LoopConfig> => {
+  let config: MonitoringConfig
+  try {
+    config = new pulumi.Config('unchained').requireObject<MonitoringConfig>('monitoring')
+  } catch (e) {
+    throw new pulumi.RunError(
+      `Could not find required configuration file. \n\tDid you copy the Pulumi.sample.yaml file to Pulumi.${pulumi.getStack()}.yaml and update the necessary configuration?`
+    )
+  }
+
+  const stackReference = new pulumi.StackReference(config.stack)
+  const kubeconfig = (await stackReference.getOutputValue('kubeconfig')) as string
+  const namespaces = (await stackReference.getOutputValue('namespaces')) as Array<string>
+  const defaultNamespace = (await stackReference.getOutputValue('defaultNamespace')) as string
+
+  const namespace = config.environment ? `${defaultNamespace}-${config.environment}` : defaultNamespace
+  if (!namespaces.includes(namespace)) {
+    throw new Error(
+      `Error: environment: ${config.environment} not found in cluster. Either remove to use default environment or verify environment exists`
+    )
+  }
+
+  const missingRequiredConfig: Array<string> = []
+
+  if (!config.stack) missingRequiredConfig.push('stack')
+
+  if (missingRequiredConfig.length) {
+    throw new Error(
+      `Missing the following configuration values from Pulumi.${pulumi.getStack()}.yaml: ${missingRequiredConfig.join(
+        ', '
+      )}`
+    )
+  }
+
+  return { kubeconfig, config, namespace }
+}
