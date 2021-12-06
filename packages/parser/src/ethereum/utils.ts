11742bd928f241b3b0f307ba15fba686d87127a5
kevin <35275952+kaladinlight@users.noreply.github.com>
2021-12-06 09:42:14 -0700

    Release 120621 (#168)
    * Ws ready check (#145)
    
    * Increase ping buffer, queue most recent subscribe data, check ready state before subscribing
    
    * chore(release): publish 3.5.0
    
    * Revert RabbitMQ Cluster (#148)
    
    * revert using rabbitmq clusters
    
    * don't remove rabbitmq cluster operator
    
    * continue use of BROKER_URI envvar
    
    * ensure rabbitmq is in sample pulumi
    
    * clean up
    
    * Fix multiple websocket subscriptions and connection handling by subscriptionId (#149)
    
    * Increase ping buffer, queue most recent subscribe data, check ready state before subscribing
    
    * chore(release): publish 3.5.0
    
    * Add worker catch and exit on main process
    
    * Working checkpoint
    
    * Use subscriptionId for queue management
    
    * Update id names to be more clear
    
    * Return on empty event
    
    * Implement unsubscribe and close for websockets (#152)
    
    * Implement unsubscribe and close for websockets
    
    * Handle close topics better
    
    * Monitoring stack (#154)
    
    * init monitoring stack
    
    * consult with Kevin
    
    * fix circleci config
    
    * yaml hell
    
    * use circleci config validator
    
    * lint
    
    * add docs
    
    * don't use logging facilities in cluster launcher
    
    * leverage new version to include prometheus scrape annotations on cluster-autoscaler and traefik
    
    * Ensure we have enough storage for verbose logs
    
    * pass secret into app as plaintext (#155)
    
    * fix grafana domain (#156)
    
    * Multiple Node Groups (#158)
    
    * update cluster-launcher to leverage multiple node groups
    
    * update pulumi config
    
    * Resource Allocation (#161)
    
    * double bitcoin indexer memory
    
    * double ingester memory
    
    * ensure api is highly available
    
    * give prometheus more juice
    
    * Transaction Parsing Module (#128)
    
    * Start transaction parsing module
    
    * Update yarn lock
    
    * Simplify trade and update tests
    
    * Don't query midgard for further thorchain details
    
    * Lint
    
    * Add caip standard (#160)
    
    * Add caip standard
    
    * pr feedback
    
    * Only aggregate values > 0
    
    * Update geth to v1.10.13 (#167)
    
    Co-authored-by: samortaylor <samortaylor@gmail.com>

---
+++ packages/parser/src/ethereum/utils.ts
@@ -0,0 +1,5 @@
+export const getSigHash = (inputData: string | undefined): string | undefined => {
+  if (!inputData) return
+  const length = inputData.startsWith('0x') ? 10 : 8
+  return inputData.substr(0, length)
+}
---
+++ packages/parser/src/types.ts
@@ -0,0 +1,56 @@
+export enum Dex {
+  Thor = 'thor',
+  Zrx = 'zrx',
+}
+
+export interface Fee {
+  caip19: string
+  value: string
+}
+
+export interface Token {
+  contract: string
+  decimals: number
+  name: string
+}
+
+export interface Trade {
+  dexName: Dex
+  memo?: string
+  type: TradeType
+}
+
+export enum TradeType {
+  Trade = 'trade',
+  Refund = 'refund',
+}
+
+export interface Transfer {
+  from: string
+  to: string
+  caip19: string
+  type: TransferType
+  totalValue: string
+  components: Array<{ value: string }>
+  token?: Token
+}
+
+export enum TransferType {
+  Send = 'send',
+  Receive = 'receive',
+}
+
+export interface Tx {
+  address: string
+  blockHash?: string
+  blockHeight: number
+  blockTime: number
+  caip2: string
+  fee?: Fee
+  trade?: Trade
+  transfers: Array<Transfer>
+  txid: string
+  value: string
+}
+
+export type TxSpecific = Partial<Pick<Tx, 'trade' | 'transfers'>>
