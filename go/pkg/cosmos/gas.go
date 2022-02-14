c306a1964b66755f5f68a182b31868b2df4f9f4a
John <cmcm3152@gmail.com>
2022-02-14 19:39:56 +0900

    WIP : test needed.

--- go/pkg/cosmos/gas.go
+++ go/pkg/cosmos/gas.go
@@ -2,24 +2,63 @@ package cosmos
 
 import (
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
 	"github.com/pkg/errors"
+	types1 "github.com/tendermint/tendermint/abci/types"
 	"strconv"
 )
 
-var encoding = NewEncoding()
-
 func (c *HTTPClient) GetGasEstimation(txBytes []byte) (string, error) {
+	var res struct {
+		GasInfo struct {
+			GasWanted string `json:"gas_wanted"`
+			GasUsed   string `json:"gas_used"`
+		} `json:"gas_info"`
+		Result struct {
+			Data   string         `json:"data"`
+			Log    string         `json:"log"`
+			Events []types1.Event `json:"events"`
+		} `json:"result"`
+	}
+	hexToUnmarshal, err := c.encoding.TxConfig.TxDecoder()(txBytes)
+	if err != nil {
+		return "", err
+	}
+	reqData, err := c.encoding.TxConfig.WrapTxBuilder(hexToUnmarshal)
+	if err != nil {
+		return "", err
+	}
+	protoProvider, ok := reqData.(authtx.ProtoTxProvider)
+	if !ok {
+		return "", err
+	}
+	protoTx := protoProvider.GetProtoTx()
 
+	_, err = c.cosmos.R().SetBody(&txtypes.SimulateRequest{Tx: protoTx}).SetResult(&res).Post("/cosmos/tx/v1beta1/simulate")
+	if err != nil {
+		return "", errors.Wrap(err, res.Result.Log)
+	}
+	if res.GasInfo.GasWanted == "" {
+		return "", errors.New("gas_wanted is empty")
+	}
+	return res.GasInfo.GasUsed, nil
 }
 
 func (c *GRPCClient) GetGasEstimation(txBytes []byte) (string, error) {
-	// TODO : How to get asterisk Tx structure
+	hexToUnmarshal, err := c.encoding.TxConfig.TxDecoder()(txBytes)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to decode tx")
+		return "", err
 	}
-	request := txtypes.SimulateRequest{Tx: decodedTx}
-
-	res, err := c.tx.Simulate(c.ctx, &request)
+	reqData, err := c.encoding.TxConfig.WrapTxBuilder(hexToUnmarshal)
+	if err != nil {
+		return "", err
+	}
+	protoProvider, ok := reqData.(authtx.ProtoTxProvider)
+	if !ok {
+		return "", err
+	}
+	protoTx := protoProvider.GetProtoTx()
+	res, err := c.tx.Simulate(c.ctx, &txtypes.SimulateRequest{Tx: protoTx})
 	if err != nil {
 		return "", errors.Wrap(err, "failed to Get transaction's gas estimation")
 	}
