48c7acdaf66e49b60e9f54941b146971c14c3d90
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2022-07-19 11:04:57 -0600

    feat: add bitcoincash coinstack

---
+++ node/coinstacks/bitcoincash/api/src/handlers.ts
@@ -0,0 +1,108 @@
+import axios from 'axios'
+import axiosRetry from 'axios-retry'
+import { bech32 } from 'bech32'
+import { Blockbook, Tx as BlockbookTx } from '@shapeshiftoss/blockbook'
+import { RPCRequest, RPCResponse } from '@shapeshiftoss/common-api'
+import { BitcoinCashTx } from './models'
+
+const INDEXER_URL = process.env.INDEXER_URL
+const INDEXER_WS_URL = process.env.INDEXER_WS_URL
+const RPC_URL = process.env.RPC_URL
+
+if (!INDEXER_URL) throw new Error('INDEXER_URL env var not set')
+if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
+if (!RPC_URL) throw new Error('RPC_URL env var not set')
+
+axiosRetry(axios, { retries: 5, retryDelay: axiosRetry.exponentialDelay })
+
+export interface NodeBlock {
+  hash: string
+  confirmations: number
+  size: number
+  strippedsize: number
+  weight: number
+  height: number
+  version: number
+  versionHex: string
+  merkleroot: string
+  tx: Array<string>
+  time: number
+  mediantime: number
+  nonce: number
+  bits: string
+  difficulty: number
+  chainwork: string
+  nTx: number
+  previousblockhash: string
+  nextblockhash: string
+}
+
+const blockbook = new Blockbook({ httpURL: INDEXER_URL, wsURL: INDEXER_WS_URL })
+
+export const formatAddress = (address: string): string => {
+  const decoded = bech32.decodeUnsafe(address.toLowerCase())
+  if (decoded?.prefix === 'ltc') return address.toLowerCase()
+  return address
+}
+
+export const handleBlock = async (hash: string): Promise<Array<BlockbookTx>> => {
+  const request: RPCRequest = {
+    jsonrpc: '2.0',
+    id: `getblock-${hash}`,
+    method: 'getblock',
+    params: [hash],
+  }
+
+  const { data } = await axios.post<RPCResponse>(RPC_URL, request)
+
+  if (data.error) throw new Error(`failed to get block: ${hash}: ${data.error.message}`)
+  if (!data.result) throw new Error(`failed to get block: ${hash}: ${JSON.stringify(data)}`)
+
+  const block = data.result as NodeBlock
+
+  // make best effort to fetch all transactions, but don't fail handling block if a single transaction fails
+  const txs = await Promise.allSettled(block.tx.map((hash) => blockbook.getTransaction(hash)))
+
+  return (txs.filter((tx) => tx.status === 'fulfilled') as Array<PromiseFulfilledResult<BlockbookTx>>).map(
+    (tx) => tx.value
+  )
+}
+
+export const handleTransaction = (tx: BlockbookTx): BitcoinCashTx => ({
+  txid: tx.txid,
+  blockHash: tx.blockHash,
+  blockHeight: tx.blockHeight,
+  timestamp: tx.blockTime,
+  confirmations: tx.confirmations,
+  value: tx.value,
+  fee: tx.fees ?? '0',
+  hex: tx.hex ?? '',
+  vin: tx.vin.map((vin) => ({
+    txid: vin.txid,
+    vout: vin.vout?.toString(),
+    sequence: vin.sequence,
+    coinbase: vin.coinbase,
+    ...(vin.hex && {
+      scriptSig: {
+        hex: vin.hex,
+      },
+    }),
+    addresses: vin.addresses,
+    value: vin.value,
+  })),
+  vout: tx.vout.map((vout) => ({
+    value: vout.value ?? '0',
+    n: vout.n,
+    ...(!vout.isAddress &&
+      vout.addresses?.length &&
+      vout.addresses[0]?.includes('OP_RETURN') && {
+        opReturn: vout.addresses[0],
+      }),
+    scriptPubKey: {
+      hex: vout.hex,
+    },
+    ...(vout.isAddress && {
+      addresses: vout.addresses ?? undefined,
+    }),
+  })),
+})
---
+++ node/coinstacks/bitcoincash/api/src/models.ts
@@ -0,0 +1,189 @@
+/* unable to import models from a module with tsoa */
+import { Account, Tx, TxHistory } from '../../../common/api/src'
+
+/**
+ * Contains info about a transaction input
+ */
+export interface Vin {
+  txid?: string
+  vout?: string
+  sequence?: number
+  coinbase?: string
+  scriptSig?: {
+    hex?: string
+  }
+  addresses?: Array<string>
+  value?: string
+}
+
+/**
+ * Contains info about a transaction output
+ */
+export interface Vout {
+  value: string
+  n: number
+  opReturn?: string
+  scriptPubKey: {
+    hex?: string
+  }
+  addresses?: Array<string>
+}
+
+/**
+ * Contains info about a BitcoinCash transaction
+ */
+export interface BitcoinCashTx extends Tx {
+  vin: Array<Vin>
+  vout: Array<Vout>
+  confirmations: number
+  value: string
+  fee: string
+  hex: string
+}
+
+/**
+ * Contains info about BitcoinCash transaction history
+ */
+export interface BitcoinCashTxHistory extends TxHistory {
+  txs: Array<BitcoinCashTx>
+}
+
+/**
+ * Contains BitcoinCash specific transaction info as returned from the node
+ */
+export interface BitcoinCashRawTx {
+  txid: string
+  hash: string
+  version: number
+  size: number
+  vsize: number
+  weight: number
+  locktime: number
+  vin: Array<{
+    txid?: string
+    vout?: number
+    sequence?: number
+    coinbase?: string
+    scriptSig?: {
+      asm: string
+      hex: string
+    }
+    txinwitness?: string
+  }>
+  vout: Array<{
+    value?: string | number
+    n?: number
+    scriptPubKey?: {
+      asm: string
+      hex: string
+      reqSigs: number
+      type: string
+      addresses: string[]
+    }
+  }>
+  hex: string
+  blockhash: string
+  confirmations: number
+  time: number
+  blocktime: number
+}
+
+/**
+ * Contains info about an unspent transaction output
+ */
+export interface Utxo {
+  txid: string
+  vout: number
+  value: string
+  height?: number
+  confirmations: number
+  address?: string
+  path?: string
+  locktime?: number
+  coinbase?: boolean
+}
+
+export interface BitcoinCashAddress {
+  balance: string
+  pubkey: string
+}
+
+/**
+ * Contains additional BitcoinCash specific account info
+ */
+export interface BitcoinCashAccount extends Account {
+  /**
+   * List of associated addresses for an xpub
+   */
+  addresses?: Array<BitcoinCashAddress>
+
+  /**
+   * The next unused receive address index for an xpub (change index 0)
+   */
+  nextReceiveAddressIndex?: number
+
+  /**
+   * The next unused change address index for an xpub (change index 1)
+   */
+  nextChangeAddressIndex?: number
+}
+
+/**
+ * Contains network fee info
+ */
+export type NetworkFee = {
+  blocksUntilConfirmation: number
+  satsPerKiloByte: number
+}
+
+/**
+ * Gets current network fee estimates
+ */
+export interface NetworkFees {
+  fast?: NetworkFee
+  average?: NetworkFee
+  slow?: NetworkFee
+}
+
+/**
+ * BitcoinCashAPI coin specific implementation
+ */
+export interface BitcoinCashAPI {
+  /**
+   * Get all unspent transaction outputs for a pubkey
+   *
+   * @param pubkey account pubkey
+   *
+   * @returns {Promise<Array<Utxo>>} account utxos
+   */
+  // @Get('account/{pubkey}/utxos')
+  getUtxos(pubkey: string): Promise<Array<Utxo>>
+
+  /**
+   * Get transaction details
+   *
+   * @param {string} txid transaction hash
+   *
+   * @returns {Promise<BitcoinCashTx>} transaction payload
+   */
+  // @Get('tx/{txid}')
+  getTransaction(txid: string): Promise<BitcoinCashTx>
+
+  /**
+   * Get raw transaction details directly from the node
+   *
+   * @param {string} txid transaction hash
+   *
+   * @returns {Promise<BitcoinCashRawTx>} transaction payload
+   */
+  // @Get('tx/{txid}/raw')
+  getRawTransaction(txid: string): Promise<BitcoinCashRawTx>
+
+  /**
+   * Get current network fees
+   *
+   * @returns {Promise<NetworkFees>} network fees
+   */
+  // @Get('fees')
+  getNetworkFees(): Promise<NetworkFees>
+}
