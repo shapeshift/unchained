cd94d91ed1ab88113ccbb25fa0f4743c91aa9897
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2022-09-30 15:34:13 -0600

    add thorchain block event processing

---
+++ go/coinstacks/thorchain/events.go
@@ -0,0 +1,97 @@
+package thorchain
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/shapeshift/unchained/pkg/cosmos"
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+)
+
+type EventOutbound struct {
+	InTxID      string `json:"in_tx_id"`
+	ID          string `json:"id"`
+	Chain       string `json:"chain"`
+	FromAddress string `json:"from"`
+	ToAddress   string `json:"to"`
+	Coin        string `json:"coin"`
+	Memo        string `json:"memo"`
+}
+
+var registry = make(map[string]func() TypedEvent)
+
+func init() {
+	registry["outbound"] = func() TypedEvent { return &EventOutbound{} }
+}
+
+type TypedEvent interface{}
+
+func ParseBlockEvents(events []abci.Event) (cosmos.EventsByMsgIndex, []TypedEvent, error) {
+	idx := 0
+	typedEvents := []TypedEvent{}
+	eventsByMsgIndex := cosmos.EventsByMsgIndex{}
+
+	for _, event := range events {
+		rt, ok := registry[event.Type]
+		if !ok {
+			continue
+		}
+
+		attrMap := make(map[string]json.RawMessage)
+		attributes := make(cosmos.ValueByAttribute)
+		for _, a := range event.Attributes {
+			attributes[string(a.Key)] = string(a.Value)
+			attrMap[string(a.Key)] = json.RawMessage(fmt.Sprintf("\"%s\"", string(a.Value)))
+		}
+
+		attrBytes, err := json.Marshal(attrMap)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		typedEvent := rt()
+		if err := tmjson.Unmarshal(attrBytes, typedEvent); err != nil {
+			return nil, nil, err
+		}
+
+		msgIndex := strconv.Itoa(idx)
+		eventsByMsgIndex[msgIndex] = make(cosmos.AttributesByEvent)
+		eventsByMsgIndex[msgIndex][event.Type] = attributes
+		typedEvents = append(typedEvents, typedEvent)
+		idx++
+	}
+
+	return eventsByMsgIndex, typedEvents, nil
+}
+
+func TypedEventsToMessages(events []TypedEvent) []cosmos.Message {
+	messages := []cosmos.Message{}
+
+	coinToValue := func(coin string) cosmos.Value {
+		coinParts := strings.Fields(coin)
+		return cosmos.Value{
+			Amount: coinParts[0],
+			Denom:  coinParts[1],
+		}
+	}
+
+	for _, event := range events {
+		switch v := event.(type) {
+		case *EventOutbound:
+			message := cosmos.Message{
+				Addresses: []string{v.FromAddress, v.ToAddress},
+				Origin:    v.FromAddress,
+				From:      v.FromAddress,
+				To:        v.ToAddress,
+				Type:      "outbound",
+				Value:     coinToValue(v.Coin),
+			}
+			messages = append(messages, message)
+		}
+	}
+
+	return messages
+}
