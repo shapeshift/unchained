5e656eccbd98ae7c4a233500d7acded96c5bb984
kevin <35275952+kaladinlight@users.noreply.github.com>
2022-11-29 11:01:21 -0700

    chore: follow up avax config tweaks (#560)

---
+++ node/coinstacks/avalanche/daemon/readiness.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SYNCING=$(curl -X POST --data '{"jsonrpc":"2.0", "id":1, "method":"eth_syncing", "params":[]}' -H 'Content-Type:application/json;' http://localhost:9650/ext/bc/C/rpc | jq -r .result)
+PEER_COUNT=$(curl -X POST --data '{"jsonrpc":"2.0", "id":1, "method":"info.peers", "params":[]}' -H 'Content-Type:application/json;' http://localhost:9650/ext/info | jq -r .result.numPeers)
+
+if [ "$SYNCING" == "false" ]; then
+  # Make sure we have peers
+  if [ "$PEER_COUNT" == "0" ]; then
+    echo "node is synced, but has 0 peer connections"
+    exit 1
+  fi
+
+  echo "node is synced, with $PEER_COUNT peers"
+  exit 0
+fi
+
+echo "node is still syncing"
+exit 1
