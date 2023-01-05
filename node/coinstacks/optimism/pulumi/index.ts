8e3016da2a8d059f3fb753ac225d9dc868dd1f95
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-01-05 10:21:25 -0700

    update blockbook config and deploy all services

--- node/coinstacks/optimism/pulumi/index.ts
+++ node/coinstacks/optimism/pulumi/index.ts
@@ -1,8 +1,8 @@
 import { parse } from 'dotenv'
 import { readFileSync } from 'fs'
 import * as k8s from '@pulumi/kubernetes'
-import { createService, deployStatefulService, getConfig, Service } from '../../../../pulumi'
-//import { api } from '../../../pulumi'
+import { createService, deployApi, deployStatefulService, getConfig, Service } from '../../../../pulumi'
+import { api } from '../../../pulumi'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Outputs = Record<string, any>
@@ -36,20 +36,20 @@ export = async (): Promise<Outputs> => {
 
   new k8s.core.v1.Secret(asset, { metadata: { name: asset, namespace }, stringData }, { provider })
 
-  //const baseImageName = 'shapeshiftdao/unchained-base:latest'
+  const baseImageName = 'shapeshiftdao/unchained-base:latest'
 
-  //await deployApi({
-  //  app: name,
-  //  asset,
-  //  baseImageName,
-  //  buildAndPushImageArgs: { context: '../api' },
-  //  config,
-  //  container: { command: ['node', `dist/${coinstack}/api/src/app.js`] },
-  //  getHash: api.getHash,
-  //  namespace,
-  //  provider,
-  //  secretEnvs: api.secretEnvs,
-  //})
+  await deployApi({
+    app: name,
+    asset,
+    baseImageName,
+    buildAndPushImageArgs: { context: '../api' },
+    config,
+    container: { command: ['node', `dist/${coinstack}/api/src/app.js`] },
+    getHash: api.getHash,
+    namespace,
+    provider,
+    secretEnvs: api.secretEnvs,
+  })
 
   if (config.statefulService) {
     const services = config.statefulService.services.reduce<Record<string, Service>>((prev, service) => {
@@ -57,7 +57,10 @@ export = async (): Promise<Outputs> => {
         prev[service.name] = createService({
           asset,
           config: service,
-          ports: { 'daemon-rpc': { port: 8545 } },
+          ports: {
+            'daemon-rpc': { port: 8545 },
+            'daemon-ws': { port: 8546, pathPrefix: '/websocket', stripPathPrefix: true },
+          },
         })
       }
 
@@ -69,27 +72,27 @@ export = async (): Promise<Outputs> => {
         })
       }
 
-      //if (service.name === 'indexer') {
-      //  prev[service.name] = createService({
-      //    asset,
-      //    config: service,
-      //    command: [
-      //      '/bin/blockbook',
-      //      '-blockchaincfg=/config.json',
-      //      '-datadir=/data',
-      //      '-sync',
-      //      '-public=:8001',
-      //      '-enablesubnewtx',
-      //      '-logtostderr',
-      //      '-debug',
-      //    ],
-      //    ports: { public: { port: 8001 } },
-      //    configMapData: { 'indexer-config.json': readFileSync('../indexer/config.json').toString() },
-      //    volumeMounts: [{ name: 'config-map', mountPath: '/config.json', subPath: 'indexer-config.json' }],
-      //    readinessProbe: { initialDelaySeconds: 20, periodSeconds: 5, failureThreshold: 12 },
-      //    livenessProbe: { timeoutSeconds: 10, initialDelaySeconds: 60, periodSeconds: 15, failureThreshold: 4 },
-      //  })
-      //}
+      if (service.name === 'indexer') {
+        prev[service.name] = createService({
+          asset,
+          config: service,
+          command: [
+            '/bin/blockbook',
+            '-blockchaincfg=/config.json',
+            '-datadir=/data',
+            '-sync',
+            '-public=:8001',
+            '-enablesubnewtx',
+            '-logtostderr',
+            '-debug',
+          ],
+          ports: { public: { port: 8001 } },
+          configMapData: { 'indexer-config.json': readFileSync('../indexer/config.json').toString() },
+          volumeMounts: [{ name: 'config-map', mountPath: '/config.json', subPath: 'indexer-config.json' }],
+          readinessProbe: { initialDelaySeconds: 20, periodSeconds: 5, failureThreshold: 12 },
+          livenessProbe: { timeoutSeconds: 10, initialDelaySeconds: 60, periodSeconds: 15, failureThreshold: 4 },
+        })
+      }
 
       return prev
     }, {})
