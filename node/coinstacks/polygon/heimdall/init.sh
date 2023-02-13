c91354e4a1459fecf2f9d528af870537acef3fc3
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-02-13 10:06:08 -0700

    start adding polygon coinstack

---
+++ node/coinstacks/polygon/heimdall/init.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+[ "$DEBUG" == "true" ] && set -x
+
+HOME_DIR=/root/.heimdalld
+CONFIG_DIR=$HOME_DIR/config
+
+# shapshots provided by: https://snapshot.polygon.technology/
+if [[ -n $SNAPSHOT && ! -f "$HOME_DIR/data/priv_validator_state.json" ]]; then
+  rm -rf $HOME_DIR/data;
+  mkdir -p $HOME_DIR/data;
+  wget -c $SNAPSHOT -O - | tar -xzf - -C $HOME_DIR/data
+fi
+
+if [[ ! -d "$CONFIG_DIR" ]]; then
+  # init chain
+  heimdalld init --home $HOME_DIR
+
+  # overwrite genesis file
+  cp /var/lib/heimdall/genesis-mainnet-v1.json $CONFIG_DIR/genesis.json
+fi
+
+start() {
+  heimdalld start \
+    --home $HOME_DIR \
+    --rpc.laddr tcp://0.0.0.0:26657 \
+    --bor_rpc_url http://localhost:8545 \
+    --eth_rpc_url $ETH_RPC_URL &
+  DAEMON_PID="$!"
+
+  heimdalld rest-server \
+    --home $HOME_DIR \
+    --node "tcp://localhost:26657" &
+  REST_SERVER_PID="$!"
+}
+
+stop() {
+  echo "Catching signal and sending to PID: $DAEMON_PID" && kill $DAEMON_PID
+  while $(kill -0 $DAEMON_PID 2>/dev/null); do sleep 1; done
+  echo "Catching signal and sending to PID: $REST_SERVER_PID" && kill $REST_SERVER_PID
+  while $(kill -0 $REST_SERVER_PID 2>/dev/null); do sleep 1; done
+}
+
+trap 'stop' TERM INT
+start
+wait $DAEMON_PID $REST_SERVER_PID
