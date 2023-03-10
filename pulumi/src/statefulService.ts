6fc10d7a7d4cd7fbf4c5cc1c312d7acbde59fcda
Łukasz Myśliński <lukasz@mlnative.com>
2023-03-10 21:02:15 +0100

    use image with increased timeout

--- pulumi/src/statefulService.ts
+++ pulumi/src/statefulService.ts
@@ -365,7 +365,7 @@ export async function deployStatefulService(
 
     const backupContainer: k8s.types.input.core.v1.Container = {
       name: `${asset}-backup-runner`,
-      image: 'lukmyslinski/backuprunner:0.8',
+      image: 'lukmyslinski/backuprunner:0.9',
       args: ['-n', namespace, '-s', `${asset}-sts`, '-p', pvcs, '-r', `${config.statefulService.replicas}`, "-c", `${backupsToKeep}`],
     }
 
