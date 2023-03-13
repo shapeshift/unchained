bb2f5e012b7567bcfd81f9b463680e79846656f3
Łukasz Myśliński <lukasz@mlnative.com>
2023-03-13 13:38:03 +0100

    One more docker image update

--- pulumi/src/cluster/sts/backup-cron.ts
+++ pulumi/src/cluster/sts/backup-cron.ts
@@ -39,7 +39,7 @@ const createBackupContainer = (asset: string, namespace: string, sts: StsDefinit
   const pvcList = getPvcNames(asset, sts.replicas, sts.services)
   return {
     name: `${asset}-backup-runner`,
-    image: 'lukmyslinski/backuprunner:0.22',
+    image: 'lukmyslinski/backuprunner:0.23',
     args: ['-n', namespace, '-s', `${asset}-sts`, '-p', pvcList, '-r', `${sts.replicas}`, "-c", `${sts.backupCount}`],
   }
 }
