686338c69500068cd169b40e03209675b04bdb82
Łukasz Myśliński <lukasz@mlnative.com>
2023-03-13 12:24:57 +0100

    more logs

--- pulumi/backuprunner/src/cleanup.ts
+++ pulumi/backuprunner/src/cleanup.ts
@@ -1,11 +1,11 @@
 import k8s, { KubernetesObject } from '@kubernetes/client-node'
 
-const sortByCreationTimestamp = 
+// const sortByCreationTimestamp = 
 
 export const cleanup = async (k8sApi: k8s.KubernetesObjectApi, sts: string, namespace: string, pvcList: string, backupCount: number) => {
   var pvcCount = pvcList.split(',').length;
-  var pvcsToKeep = pvcCount*backupCount;
-  console.log(`Backup count to keep - ${backupCount}. ${sts} consists of ${pvcCount} PVC's, so will keep ${pvcsToKeep} latest snapshots`)
+  var pvcsToKeepCount = pvcCount*backupCount;
+  console.log(`Backup count to keep - ${backupCount}. ${sts} consists of ${pvcCount} PVC's, so will keep ${pvcsToKeepCount} latest snapshots`)
 
   const snapshots = await k8sApi.list("snapshot.storage.k8s.io/v1", "VolumeSnapshot", namespace, undefined, undefined, undefined, undefined, `statefulset=${sts}`);
   const items = snapshots.body.items
@@ -13,25 +13,26 @@ export const cleanup = async (k8sApi: k8s.KubernetesObjectApi, sts: string, name
 
   console.log(items[0].metadata)
   console.log(typeof(items[0].metadata))
-  console.log(typeof(items[0].metadata?.creationTimestamp))
+  console.log(items[0].metadata!!.creationTimestamp!!)
+  console.log(new Date(items[0].metadata!!.creationTimestamp!!).getTime())
   console.log(items[0].metadata?.creationTimestamp)
 
-  var sorted = items.sort((a, b) => (a.metadata?.creationTimestamp?.getTime() || 0 - (b.metadata?.creationTimestamp?.getTime() || 0)));
+  var sorted = items.sort((a, b) => (new Date(a.metadata!!.creationTimestamp!!).getTime()  - new Date(b.metadata!!.creationTimestamp!!).getTime()));
 
   sorted.forEach(x => console.log(x))
 
-  if(sorted.length <= pvcsToKeep){
+  if(sorted.length <= pvcsToKeepCount){
     console.log(`Not archiving old snapshots, too few results`)
     return;
   }
 
-  var [toKeep, toRemove] = [sorted.slice(0, pvcsToKeep), sorted.slice(pvcsToKeep)]
+  var [toKeep, toRemove] = [sorted.slice(0, pvcsToKeepCount), sorted.slice(pvcsToKeepCount)]
 
-  console.log("Volume snapshots to keep: ")
-  toKeep.forEach(x => console.log(x))
+  console.log("Volume snapshots to keep: ", toKeep.length)
+  toKeep.forEach(x => console.log(x.metadata?.name, x.metadata?.creationTimestamp))
 
-  console.log("Volume snapshots to remove: ")
-  toRemove.forEach(x => console.log(x))
+  console.log("Volume snapshots to remove: ", toRemove.length)
+  toRemove.forEach(x => console.log(x.metadata?.name, x.metadata?.creationTimestamp))
 
 
 
