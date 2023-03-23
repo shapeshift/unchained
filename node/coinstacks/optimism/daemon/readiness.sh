31c9028b3887c25a2d2b49eb3e890f4098413ff6
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-03-23 09:06:18 -0600

    hotfix: new alchemy api key

--- node/coinstacks/optimism/daemon/readiness.sh
+++ node/coinstacks/optimism/daemon/readiness.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TARGET_HEIGHT_HEX=$(curl -s -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' https://opt-mainnet.g.alchemy.com/v2/Im6I9y7QNPR3v5kaj73xrm4F99mcuFvG -H 'Content-Type: application/json' | jq -r .result)
+TARGET_HEIGHT_HEX=$(curl -s -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' https://opt-mainnet.g.alchemy.com/v2/CYDzKJ-NTZT16TzZf6j17ZB95UsA_nUA -H 'Content-Type: application/json' | jq -r .result)
 LOCAL_HEIGHT_HEX=$(curl -s -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://localhost:8545 -H 'Content-Type: application/json' | jq -r .result)
 TARGET_HEIGHT=$(echo $(($TARGET_HEIGHT_HEX)))
 LOCAL_HEIGHT=$(echo $(($LOCAL_HEIGHT_HEX)))
