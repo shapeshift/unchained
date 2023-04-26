e31484e6ba200cfffadea87976fecf57723693fd
Łukasz Myśliński <lukasz@mlnative.com>
2023-04-26 16:16:51 +0200

    updated domain config

--- monitoring/src/config.ts
+++ monitoring/src/config.ts
@@ -3,13 +3,13 @@ import * as pulumi from '@pulumi/pulumi'
 export interface MonitoringConfig {
   stack: string
   environment: string
+  domain: string
 }
 
 export interface LoopConfig {
   kubeconfig: string
   config: MonitoringConfig
   namespace: string
-  domain: string
 }
 
 export const getConfig = async (): Promise<LoopConfig> => {
@@ -26,7 +26,19 @@ export const getConfig = async (): Promise<LoopConfig> => {
   const kubeconfig = (await stackReference.getOutputValue('kubeconfig')) as string
   const namespaces = (await stackReference.getOutputValue('namespaces')) as Array<string>
   const defaultNamespace = (await stackReference.getOutputValue('defaultNamespace')) as string
-  const domain = (await stackReference.getOutputValue('rootDomainName')) as string
+
+  const missingRequiredConfig: Array<string> = []
+
+  if (!config.stack) missingRequiredConfig.push('stack')
+  if (!config.domain) missingRequiredConfig.push('domain')
+
+  if (missingRequiredConfig.length) {
+    throw new Error(
+      `Missing the following configuration values from Pulumi.${pulumi.getStack()}.yaml: ${missingRequiredConfig.join(
+        ', '
+      )}`
+    )
+  }
 
   const namespace = config.environment ? `${defaultNamespace}-${config.environment}` : defaultNamespace
   if (!namespaces.includes(namespace)) {
@@ -35,5 +47,5 @@ export const getConfig = async (): Promise<LoopConfig> => {
     )
   }
 
-  return { kubeconfig, config, namespace, domain }
+  return { kubeconfig, config, namespace }
 }
