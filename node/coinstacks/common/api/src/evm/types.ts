5738dac3ccae7e49b2cb5ed8c5dadc9d916316b9
Łukasz Myśliński <lukasz@mlnative.com>
2023-06-02 18:30:12 +0200

    type cleanup

--- node/coinstacks/common/api/src/evm/types.ts
+++ node/coinstacks/common/api/src/evm/types.ts
@@ -29,27 +29,6 @@ export interface NodeBlock {
   uncles: Array<string>
 }
 
-// action: {
-//   callType: 'call',
-//   from: '0xd7f1dd5d49206349cae8b585fcb0ce3d96f1696f',
-//   gas: '0xcb07',
-//   input: '0xa9059cbb0000000000000000000000003728eca61872179220b806cb8c819ed6eefeb42500000000000000000000000000000000000000000000000000000000000003e2',
-//   to: '0xddafbb505ad214d7b80b1f830fccc89b60fb7a83',
-//   value: '0x0'
-// },
-// blockHash: '0x1946bbe1192c9af842a15871d18da5ef510ae19636bb2a7143e79e5f5d2d6642',
-// blockNumber: 28254455,
-// result: {
-//   gasUsed: '0x25f3',
-//   output: '0x0000000000000000000000000000000000000000000000000000000000000001'
-// },
-// subtraces: 1,
-// traceAddress: [ 4 ],
-// transactionHash: '0x7a418a4567e3d9a97aa8c7a93c8daa988b85cd12a174db94811cbf3dd1e660a4',
-// transactionPosition: 1,
-// type: 'call'
-// },
-
 export interface TraceCall {
   action: {
     callType: string
