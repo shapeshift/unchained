9866907d483584014fc420f77681a611e8e4704a
Lukasz Myslinski <lukasz@mlnative.com>
2023-06-09 15:41:15 +0200

    Logic cleanup, valid dns name

--- node/coinstacks/bnbsmartchain/daemon/init.sh
+++ node/coinstacks/bnbsmartchain/daemon/init.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash 
+
+set -e
+
+[ "$DEBUG" == "true" ] && set -x
 
 # Download the config
 wget   $(curl -s https://api.github.com/repos/bnb-chain/bsc/releases/latest |grep browser_ |grep mainnet |cut -d\" -f4)
@@ -19,25 +23,30 @@ if [ ! -d "$CHAINDATA_DIR" ]; then
     geth init --datadir $DATA_DIR genesis.json
 fi
 
+geth snapshot insecure-prune-all --datadir $DATA_DIR ./genesis.json
 
 start() {
-  geth --tries-verify-mode none --config ./config.toml \
-  --datadir $DATA_DIR \
-  --syncmode snap \
-  --http \
-  --http.addr 0.0.0.0 \
-  --http.port 8545 \
-  --http.api eth,net,web3,debug,txpool,parlia \
-  --http.vhosts '*' \
-  --http.corsdomain '*' \
-  --ws \
-  --ws.port 8546 \
-  --ws.api eth,net,web3,debug,txpool,parlia \
-  --ws.origins '*' \
-  --rpc.allow-unprotected-txs \
-  --txlookuplimit 0 \
-  --cache 8000 \
-  --nat none &
+  geth \
+    --tries-verify-mode none \
+    --syncmode snap \
+    --config config.toml \
+    --datadir $DATA_DIR \
+    --http \
+    --http.addr 0.0.0.0 \
+    --http.port 8545 \
+    --http.api eth,net,web3,debug,txpool,parlia \
+    --http.vhosts '*' \
+    --http.corsdomain '*' \
+    --ws \
+    --ws.port 8546 \
+    --ws.api eth,net,web3,debug,txpool,parlia \
+    --ws.origins '*' \
+    --syncmode full \
+    --maxpeers 200 \
+    --rpc.allow-unprotected-txs \
+    --txlookuplimit 0 \
+    --cache 8000 \
+    --nat none &
   PID="$!"
 
   hard_reset_peers &
