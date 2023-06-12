61fe3546befab4802435e9198a15d50bade899a0
Łukasz Myśliński <lukasz@mlnative.com>
2023-06-12 21:47:15 +0200

    Cleanup readiness script

--- node/coinstacks/arbitrum/daemon/readiness.sh
+++ node/coinstacks/arbitrum/daemon/readiness.sh
@@ -3,15 +3,9 @@
 set -e
 
 SYNCING=$(curl -s -d '{"jsonrpc":"2.0", "id":1, "method":"eth_syncing", "params":[]}' -H 'Content-Type:application/json;' http://localhost:8545 | jq -r .result)
-PEER_COUNT=$(curl -s -d '{"jsonrpc":"2.0", "id":1, "method":"net_peerCount", "params":[]}' -H 'Content-Type:application/json;' http://localhost:8545 | jq -r .result)
 
 # check if node is reporting it is synced
 if [[ $SYNCING == "false" ]]; then
-  # make sure we have peers
-  if [[ $PEER_COUNT == "0" ]]; then
-    echo "node is synced, but has 0 peer connections"
-    exit 1
-  fi
 
   TARGET_HEIGHT_HEX=$(curl -s -d '{"jsonrpc":"2.0", "id":1, "method":"eth_blockNumber", "params":[]}' -H 'Content-Type:application/json;' https://arb1.arbitrum.io/rpc/ | jq -r .result)
   TARGET_HEIGHT=$(echo $(($TARGET_HEIGHT_HEX)))
@@ -22,10 +16,10 @@ if [[ $SYNCING == "false" ]]; then
   # the node can incorrectly report it is synced after catch up, but then falling back out of sync
   # validate against a public reference node to confirm sync status
   if [[ $LOCAL_HEIGHT -ge $NOMINAL_HEIGHT ]]; then
-    echo "node is synced, with $PEER_COUNT peers"
+    echo "node is synced"
     exit 0
   else
-    echo "node is reporting synced, with $PEER_COUNT peers, but is behind public reference node (Local: $LOCAL_HEIGHT, Reference: $TARGET_HEIGHT)"
+    echo "node is reporting synced, but is behind public reference node (Local: $LOCAL_HEIGHT, Reference: $TARGET_HEIGHT)"
     exit 1
   fi
 fi
