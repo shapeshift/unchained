38e3d5a809908641d32ebde67029c1aae1e08862
Łukasz Myśliński <lukasz@mlnative.com>
2023-06-12 10:39:31 +0200

    Skip snapshot validation

--- node/coinstacks/arbitrum/daemon/init.sh
+++ node/coinstacks/arbitrum/daemon/init.sh
@@ -13,14 +13,14 @@ if [ -n "$SNAPSHOT_FILE" ]; then
 fi
 
 # TODO skip this part if the node was already bootstrapped
-ACTUAL_CHECKSUM=$(sha256sum "$SNAPSHOT_FILE" | awk '{ print $1 }')
-while [ "$ACTUAL_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; do
-  echo "Invalid checksum, redownloading the snapshot"
+# ACTUAL_CHECKSUM=$(sha256sum "$SNAPSHOT_FILE" | awk '{ print $1 }')
+# while [ "$ACTUAL_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; do
+  # echo "Invalid checksum, redownloading the snapshot"
   curl -o $SNAPSHOT_FILE -L -O --retry 999 --retry-max-time 0 -C - $SNAPSHOT_URL
-  ACTUAL_CHECKSUM=$(sha256sum "$SNAPSHOT_FILE" | awk '{ print $1 }')
-done
+  # ACTUAL_CHECKSUM=$(sha256sum "$SNAPSHOT_FILE" | awk '{ print $1 }')
+# done
 
-echo "Snaphot is valid"
+# echo "Snaphot is valid"
 
 # docker run --rm -it  -v /some/local/dir/arbitrum:/home/user/.arbitrum -p 0.0.0.0:8547:8547 -p 0.0.0.0:8548:8548 offchainlabs/nitro-node:v2.0.14-2baa834 --l1.url https://l1-node:8545 --l2.chain-id=<L2ChainId> --http.api=net,web3,eth,debug --http.corsdomain=* --http.addr=0.0.0.0 --http.vhosts=*
 
