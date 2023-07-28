486b939c16b4aa821fdc8218b966495fbd81281b
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-07-28 11:38:52 -0600

    small update to blockbook readiness probe

--- node/packages/blockbook/scripts/readiness.sh
+++ node/packages/blockbook/scripts/readiness.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-IN_SYNC=$(curl -s --connect-timeout 10 http://localhost:8001/api/v2 -H 'Content-Type: application/json')
-IN_SYNC_RESULT=$(echo "$IN_SYNC" | jq -r .blockbook.inSync)
+STATUS=$(curl -sf http://localhost:8001/api/v2) || exit 1
+IN_SYNC=$(echo $STATUS | jq -r '.blockbook.inSync')
 
-if [[ $IN_SYNC_RESULT == "true" ]]; then
-    echo "Blockbook is synced"
+if [[ $IN_SYNC == "true" ]]; then
+    echo "blockbook is synced"
     exit 0
-elif [[ $IN_SYNC_RESULT == "false" ]]; then
-    echo "Blockbook is still syncing"
-    exit 1
-else
-    echo "Blockbook error: ${IN_SYNC}"
-    exit 1
 fi
+
+echo "blockbook is still syncing"
+exit 1
