de6a5048850c2189eca05259faea8df2ac91cc3f
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-08-08 11:11:20 -0600

    chore: reduce blockbook workers to 1 after initial sync

--- node/coinstacks/ethereum/pulumi/index.ts
+++ node/coinstacks/ethereum/pulumi/index.ts
@@ -55,6 +55,7 @@ export = async (): Promise<Outputs> => {
         return {
           ...service,
           ...defaultBlockbookServiceArgs,
+          command: [...defaultBlockbookServiceArgs.command, 'workers=1'],
           configMapData: { 'indexer-config.json': readFileSync('../indexer/config.json').toString() },
         }
       default:
