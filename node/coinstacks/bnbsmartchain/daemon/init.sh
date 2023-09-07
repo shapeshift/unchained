c5eab3aa452153acf3af2fe3a579e2ce0674da77
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-09-07 14:15:45 -0600

    fix: omit 0 gas price and allow 1 gwei broadcast

--- node/coinstacks/bnbsmartchain/daemon/init.sh
+++ node/coinstacks/bnbsmartchain/daemon/init.sh
@@ -76,6 +76,7 @@ start() {
     --syncmode full \
     --maxpeers 200 \
     --rpc.allow-unprotected-txs \
+    --txpool.pricelimit 1 \
     --txlookuplimit 0 \
     --cache 8000 \
     --nat none &
