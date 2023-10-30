b80f63de3123c03b80e3181d293b8aeb8fe41de9
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-10-30 12:30:15 -0600

    feat: add arbitrum nova coinstack

---
+++ node/coinstacks/arbitrum-nova/daemon/init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+[ "$DEBUG" = "true" ] && set -x
+
+start() {
+  /usr/local/bin/nitro \
+  --chain.id 42170 \
+  --chain.name nova \
+  --parent-chain.connection.url $L1_RPC_ENDPOINT \
+  --init.url 'https://snapshot.arbitrum.foundation/nova/nitro-pruned.tar' \
+  --init.download-path /data/tmp \
+  --persistent.chain /data \
+  --auth.jwtsecret /jwt.hex \
+  --http.addr 0.0.0.0 \
+  --http.port 8547 \
+  --http.api eth,net,web3,debug,txpool,arb \
+  --http.vhosts '*' \
+  --http.corsdomain '*' \
+  --ws.addr 0.0.0.0 \
+  --ws.port 8548 \
+  --ws.api eth,net,web3,debug,txpool,arb \
+  --ws.origins '*' \
+  --node.staker.enable='false' \
+  --node.tx-lookup-limit 0 &
+  PID="$!"
+}
+
+stop() {
+  echo "Catching signal and sending to PID: $PID" && kill $PID
+  while $(kill -0 $PID 2>/dev/null); do sleep 1; done
+}
+
+trap 'stop' TERM INT
+start
+wait $PID
---
+++ node/coinstacks/arbitrum-nova/pulumi/index.ts
@@ -0,0 +1,58 @@
+import { readFileSync } from 'fs'
+import { deployCoinstack } from '../../../../pulumi/src/coinstack'
+import { Outputs, CoinServiceArgs, getConfig } from '../../../../pulumi/src'
+import { defaultBlockbookServiceArgs } from '../../../packages/blockbook/src/constants'
+
+//https://www.pulumi.com/docs/intro/languages/javascript/#entrypoint
+export = async (): Promise<Outputs> => {
+  const appName = 'unchained'
+  const coinstack = 'arbitrum-nova'
+  const sampleEnv = readFileSync('../sample.env')
+  const { kubeconfig, config, namespace } = await getConfig()
+
+  const coinServiceArgs = config.statefulService?.services?.map((service): CoinServiceArgs => {
+    switch (service.name) {
+      case 'daemon':
+        return {
+          ...service,
+          ports: {
+            'daemon-http': { port: 8547 },
+            'daemon-ws': { port: 8548, pathPrefix: '/websocket', stripPathPrefix: true },
+          },
+          env: {
+            L1_RPC_ENDPOINT: `http://ethereum-svc.${namespace}.svc.cluster.local:8545`,
+          },
+          configMapData: {
+            'jwt.hex': readFileSync('../daemon/jwt.hex').toString(),
+            'evm.sh': readFileSync('../../../scripts/evm.sh').toString(),
+          },
+          volumeMounts: [
+            { name: 'config-map', mountPath: '/jwt.hex', subPath: 'jwt.hex' },
+            { name: 'config-map', mountPath: '/evm.sh', subPath: 'evm.sh' },
+          ],
+          startupProbe: { periodSeconds: 30, failureThreshold: 60, timeoutSeconds: 10 },
+          livenessProbe: { periodSeconds: 30, failureThreshold: 5, timeoutSeconds: 10 },
+          readinessProbe: { periodSeconds: 30, failureThreshold: 10, timeoutSeconds: 10 },
+        }
+      case 'indexer':
+        return {
+          ...service,
+          ...defaultBlockbookServiceArgs,
+          configMapData: { 'indexer-config.json': readFileSync('../indexer/config.json').toString() },
+        }
+      default:
+        throw new Error(`no support for coin service: ${service.name}`)
+    }
+  })
+
+  return deployCoinstack({
+    appName,
+    coinServiceArgs,
+    coinstack,
+    coinstackType: 'node',
+    config,
+    kubeconfig,
+    namespace,
+    sampleEnv,
+  })
+}
