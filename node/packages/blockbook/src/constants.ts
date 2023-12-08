b5ff44d838c264c55b23893193275c7c65e63ca2
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-12-08 11:13:25 -0700

    dbcache did nothing, but workers prevents oom during large sync delta so add back

--- node/packages/blockbook/src/constants.ts
+++ node/packages/blockbook/src/constants.ts
@@ -7,7 +7,7 @@ export const defaultBlockbookServiceArgs = {
     '-public=:8001',
     '-enablesubnewtx',
     '-logtostderr',
-    '-dbcache=268435456',
+    '-workers=1',
   ],
   ports: { public: { port: 8001 } },
   volumeMounts: [{ name: 'config-map', mountPath: '/config.json', subPath: 'indexer-config.json' }],
