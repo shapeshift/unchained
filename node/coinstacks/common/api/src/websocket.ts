529d47ebffb4a35e2ff3b9c34adba91654ac9ac4
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-12-13 17:46:31 -0700

    pass logger to websocket connection handler and small cleanup

--- node/coinstacks/common/api/src/websocket.ts
+++ node/coinstacks/common/api/src/websocket.ts
@@ -42,18 +42,18 @@ export class ConnectionHandler {
   private readonly websocket: WebSocket
   private readonly registry: Registry
   private readonly prometheus: Prometheus
+  private readonly logger: Logger
   private readonly routes: Record<Topics, Methods>
-  private readonly pingInterval = 10000
+  private readonly pingIntervalMs = 10000
 
   private pingTimeout?: NodeJS.Timeout
   private subscriptionIds = new Map<string, void>()
 
-  private logger = new Logger({ namespace: ['unchained', 'coinstacks', 'common', 'api'], level: process.env.LOG_LEVEL })
-
-  private constructor(websocket: WebSocket, registry: Registry, prometheus: Prometheus) {
+  private constructor(websocket: WebSocket, registry: Registry, prometheus: Prometheus, logger: Logger) {
     this.clientId = v4()
     this.registry = registry
     this.prometheus = prometheus
+    this.logger = logger.child({ namespace: ['websocket'] })
     this.routes = {
       txs: {
         subscribe: (subscriptionId: string, data?: TxsTopicData) => this.handleSubscribeTxs(subscriptionId, data),
@@ -61,30 +61,31 @@ export class ConnectionHandler {
       },
     }
 
-    const interval = setInterval(() => {
-      this.websocket.ping()
-    }, this.pingInterval)
-
-    this.heartbeat()
-
+    this.pingTimeout = undefined
+    this.prometheus.metrics.websocketCount.inc()
     this.websocket = websocket
+    this.websocket.ping()
+
+    const pingInterval = setInterval(() => {
+      this.websocket.ping()
+    }, this.pingIntervalMs)
+
     this.websocket.onerror = (error) => {
       this.logger.error({ clientId: this.clientId, error, fn: 'ws.onerror' }, 'websocket error')
-      this.close(interval)
+      this.close(pingInterval)
     }
     this.websocket.onclose = ({ code, reason }) => {
       this.prometheus.metrics.websocketCount.dec()
       this.logger.debug({ clientId: this.clientId, code, reason, fn: 'ws.close' }, 'websocket closed')
-      this.close(interval)
+      this.close(pingInterval)
     }
     this.websocket.on('pong', () => this.heartbeat())
     this.websocket.on('ping', () => this.websocket.pong())
     this.websocket.onmessage = (event) => this.onMessage(event)
   }
 
-  static start(websocket: WebSocket, registry: Registry, prometheus: Prometheus): void {
-    prometheus.metrics.websocketCount.inc()
-    new ConnectionHandler(websocket, registry, prometheus)
+  static start(websocket: WebSocket, registry: Registry, prometheus: Prometheus, logger: Logger): void {
+    new ConnectionHandler(websocket, registry, prometheus, logger)
   }
 
   private heartbeat(): void {
@@ -93,9 +94,9 @@ export class ConnectionHandler {
     }
 
     this.pingTimeout = setTimeout(() => {
-      this.logger.debug({ fn: 'pingTimeout' }, 'heartbeat failed')
+      this.logger.debug({ clientId: this.clientId, fn: 'pingTimeout' }, 'heartbeat failed')
       this.websocket.terminate()
-    }, this.pingInterval + 1000)
+    }, this.pingIntervalMs + 1000)
   }
 
   private sendError(message: string, subscriptionId: string): void {
@@ -130,7 +131,7 @@ export class ConnectionHandler {
         }
       }
     } catch (err) {
-      this.logger.error(err, { fn: 'onMessage', event }, 'Error processing message')
+      this.logger.error(err, { clientId: this.clientId, fn: 'onMessage', event }, 'Error processing message')
     }
   }
 
