48708f119dd9f3f73aa53fda5e2acdb7dfcc54a7
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-01-17 13:04:21 -0700

    remove rpc url from utxo coins

--- node/coinstacks/dogecoin/api/src/controller.ts
+++ node/coinstacks/dogecoin/api/src/controller.ts
@@ -5,11 +5,9 @@ import { Logger } from '@shapeshiftoss/logger'
 
 const INDEXER_URL = process.env.INDEXER_URL
 const INDEXER_WS_URL = process.env.INDEXER_WS_URL
-const RPC_URL = process.env.RPC_URL
 
 if (!INDEXER_URL) throw new Error('INDEXER_URL env var not set')
 if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
-if (!RPC_URL) throw new Error('RPC_URL env var not set')
 
 export const logger = new Logger({
   namespace: ['unchained', 'coinstacks', 'dogecoin', 'api'],
@@ -24,7 +22,7 @@ const isXpub = (pubkey: string): boolean => {
 
 export const formatAddress = (address: string): string => address
 
-export const service = new Service({ blockbook, rpcUrl: RPC_URL, isXpub, addressFormatter: formatAddress })
+export const service = new Service({ blockbook, isXpub, addressFormatter: formatAddress })
 
 // assign service to be used for all instances of UTXO
 UTXO.service = service
--- node/coinstacks/litecoin/api/src/controller.ts
+++ node/coinstacks/litecoin/api/src/controller.ts
@@ -6,11 +6,9 @@ import { Logger } from '@shapeshiftoss/logger'
 
 const INDEXER_URL = process.env.INDEXER_URL
 const INDEXER_WS_URL = process.env.INDEXER_WS_URL
-const RPC_URL = process.env.RPC_URL
 
 if (!INDEXER_URL) throw new Error('INDEXER_URL env var not set')
 if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
-if (!RPC_URL) throw new Error('RPC_URL env var not set')
 
 export const logger = new Logger({
   namespace: ['unchained', 'coinstacks', 'litecoin', 'api'],
@@ -29,7 +27,7 @@ export const formatAddress = (address: string): string => {
   return address
 }
 
-export const service = new Service({ blockbook, rpcUrl: RPC_URL, isXpub, addressFormatter: formatAddress })
+export const service = new Service({ blockbook, isXpub, addressFormatter: formatAddress })
 
 // assign service to be used for all instances of UTXO
 UTXO.service = service
