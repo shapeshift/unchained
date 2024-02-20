75da27357793a8587fa2853a98d977cbf1d44636
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-02-20 13:17:00 -0700

    registry support both trace by block and trace by transaction return data

--- node/coinstacks/common/api/src/registry.ts
+++ node/coinstacks/common/api/src/registry.ts
@@ -1,16 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Logger } from '@shapeshiftoss/logger'
 import { ConnectionHandler } from './websocket'
 
 export type AddressFormatter = (address: string) => string
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export type BlockHandler<T = any, T2 = any> = (block: T) => Promise<{ txs: T2 }>
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export type TransactionHandler<T = any, T2 = any> = (tx: T) => Promise<{ addresses: Array<string>; tx: T2 }>
 
 export interface RegistryArgs {
   addressFormatter?: AddressFormatter
   blockHandler: BlockHandler
   transactionHandler: TransactionHandler
+}
+
+const isTxWithAddresses = (tx: unknown): tx is { addresses: Array<string>; tx: any } => {
+  return 'addresses' in (tx as { addresses: string; tx: any })
 }
 
 /**
@@ -117,7 +120,13 @@ export class Registry {
     try {
       const { txs } = await this.handleBlock(msg)
 
-      txs.forEach((tx: unknown) => this.onTransaction(tx))
+      txs.forEach((tx: unknown) => {
+        if (isTxWithAddresses(tx)) {
+          this.publishTransaction(tx.addresses, tx.tx)
+        } else {
+          this.onTransaction(tx)
+        }
+      })
     } catch (err) {
       this.logger.error(err, 'failed to handle block')
     }
@@ -128,19 +137,22 @@ export class Registry {
 
     try {
       const { addresses, tx } = await this.handleTransaction(msg)
-
-      addresses.forEach((address) => {
-        address = this.formatAddress(address)
-
-        if (!this.addresses[address]) return
-
-        for (const [id, connection] of this.addresses[address].entries()) {
-          const { subscriptionId } = Registry.fromId(id)
-          connection.publish(subscriptionId, address, tx)
-        }
-      })
+      this.publishTransaction(addresses, tx)
     } catch (err) {
       this.logger.error(err, 'failed to handle transaction')
     }
+  }
+
+  private publishTransaction(addresses: string[], tx: any) {
+    addresses.forEach((address) => {
+      address = this.formatAddress(address)
+
+      if (!this.addresses[address]) return
+
+      for (const [id, connection] of this.addresses[address].entries()) {
+        const { subscriptionId } = Registry.fromId(id)
+        connection.publish(subscriptionId, address, tx)
+      }
+    })
   }
 }
