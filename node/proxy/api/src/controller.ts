a38eddc13de2a121b3bba23dcbce0930620f618f
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-04-03 12:19:04 -0600

    add common proxy api

---
+++ node/proxy/api/src/controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Example, Get, Path, Response, Route, Tags } from 'tsoa'
+import { BadRequestError, InternalServerError, ValidationError } from '../../../coinstacks/common/api/src' // unable to import models from a module with tsoa
+import { ValidationResult } from './models'
+import * as elliptic from './elliptic'
+import { handleError } from '@shapeshiftoss/common-api'
+
+@Route('api/v1')
+export class Proxy extends Controller {
+  /**
+   * Get address validation status
+   *
+   * @param {string} address address
+   *
+   * @returns {Promise<Tx>} transaction payload
+   */
+  @Example<ValidationResult>({ valid: true })
+  @Response<BadRequestError>(400, 'Bad Request')
+  @Response<ValidationError>(422, 'Validation Error')
+  @Response<InternalServerError>(500, 'Internal Server Error')
+  @Tags('Validation')
+  @Get('/validate/{address}')
+  async validateAddress(@Path() address: string): Promise<ValidationResult> {
+    try {
+      return await elliptic.validateAddress(address)
+    } catch (err) {
+      throw handleError(err)
+    }
+  }
+}
---
+++ node/proxy/api/src/elliptic.ts
@@ -0,0 +1,55 @@
+import { isAxiosError } from 'axios'
+import { AML } from 'elliptic-sdk'
+
+const ELLIPTIC_API_KEY = process.env.ELLIPTIC_API_KEY
+const ELLIPTIC_API_SECRET = process.env.ELLIPTIC_API_SECRET
+
+if (!ELLIPTIC_API_KEY) throw new Error('ELLIPTIC_API_KEY env var not set')
+if (!ELLIPTIC_API_SECRET) throw new Error('ELLIPTIC_API_SECRET env var not set')
+
+const RISK_SCORE_THRESHOLD = 1
+
+const aml = new AML({
+  key: ELLIPTIC_API_KEY,
+  secret: ELLIPTIC_API_SECRET,
+})
+
+type WalletResponse = {
+  id: string
+  risk_score?: number
+}
+
+type WalletErrorResponse = {
+  name: string
+  message: string
+}
+
+export const validateAddress = async (address: string): Promise<{ valid: boolean }> => {
+  try {
+    const { data } = await aml.client.post<WalletResponse>('/v2/wallet/synchronous', {
+      subject: {
+        asset: 'holistic',
+        blockchain: 'holistic',
+        type: 'address',
+        hash: address,
+      },
+      type: 'wallet_exposure',
+    })
+
+    if (data.risk_score && data.risk_score >= RISK_SCORE_THRESHOLD) return { valid: false }
+
+    return { valid: true }
+  } catch (err) {
+    // the submitted address has not yet been processed into the elliptic tool or does not exist on the blockchain
+    // assume valid and put responsibility back on client for any further validation
+    if (
+      isAxiosError<WalletErrorResponse>(err) &&
+      err.response?.status === 404 &&
+      err.response.data.name === 'NotInBlockchain'
+    ) {
+      return { valid: true }
+    }
+
+    throw err
+  }
+}
