2aa8479e00d2a4f349e0ce6a00716b73f2f4211f
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-08-07 09:44:06 -0600

    feat: thorchain v2 support

--- go/coinstacks/thorchain/pulumi/index.ts
+++ go/coinstacks/thorchain/pulumi/index.ts
@@ -16,7 +16,7 @@ export = async (): Promise<Outputs> => {
           ...service,
           dataDir: '/root',
           env: {
-            CHAIN_ID: 'thorchain-mainnet-v1',
+            CHAIN_ID: 'thorchain-mainnet-v2',
             NET: config.network,
           },
           ports: {
