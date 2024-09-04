27010f3bdf4d2876c27440c5f1df18a083a5d1a7
kevin <35275952+kaladinlight@users.noreply.github.com>
2024-09-04 14:12:33 -0600

    feat: thorchain-1 support (#1016)

--- go/cmd/thorchain/main.go
+++ go/cmd/thorchain/main.go
@@ -23,9 +23,11 @@ var (
 )
 
 type Config struct {
-	LCDURL string `mapstructure:"LCD_URL"`
-	RPCURL string `mapstructure:"RPC_URL"`
-	WSURL  string `mapstructure:"WS_URL"`
+	LCDURL   string `mapstructure:"LCD_URL"`
+	LCDV1URL string `mapstructure:"LCD_V1_URL"`
+	RPCURL   string `mapstructure:"RPC_URL"`
+	RPCV1URL string `mapstructure:"RPC_V1_URL"`
+	WSURL    string `mapstructure:"WS_URL"`
 }
 
 func main() {
@@ -37,7 +39,7 @@ func main() {
 
 	conf := &Config{}
 	if *envPath == "" {
-		if err := config.LoadFromEnv(conf, "LCD_URL", "RPC_URL", "WS_URL"); err != nil {
+		if err := config.LoadFromEnv(conf, "LCD_URL", "LCD_V1_URL", "RPC_URL", "RPC_V1_URL", "WS_URL"); err != nil {
 			logger.Panicf("failed to load config from env: %+v", err)
 		}
 	} else {
@@ -59,9 +61,24 @@ func main() {
 		WSURL:             conf.WSURL,
 	}
 
+	cfgV1 := cosmos.Config{
+		Bech32AddrPrefix:  "thor",
+		Bech32PkPrefix:    "thorpub",
+		Bech32ValPrefix:   "thorv",
+		Bech32PkValPrefix: "thorvpub",
+		Encoding:          encoding,
+		LCDURL:            conf.LCDV1URL,
+		RPCURL:            conf.RPCV1URL,
+	}
+
 	prometheus := metrics.NewPrometheus("thorchain")
 
 	httpClient, err := cosmos.NewHTTPClient(cfg)
+	if err != nil {
+		logger.Panicf("failed to create new http client: %+v", err)
+	}
+
+	httpClientV1, err := cosmos.NewHTTPClient(cfgV1)
 	if err != nil {
 		logger.Panicf("failed to create new http client: %+v", err)
 	}
@@ -76,7 +93,7 @@ func main() {
 		logger.Panicf("failed to create new websocket client: %+v", err)
 	}
 
-	indexer := api.NewAffiliateFeeIndexer(cfg, httpClient)
+	indexer := api.NewAffiliateFeeIndexer(cfg, []*cosmos.HTTPClient{httpClientV1, httpClient})
 	if err := indexer.Sync(); err != nil {
 		logger.Panicf("failed to index affiliate fees: %+v", err)
 	}
