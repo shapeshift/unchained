c8ff6cf81f604abdd8ce282b16dfba06de2d313f
kevin <35275952+kaladinlight@users.noreply.github.com>
2024-09-24 12:51:23 -0600

    chore: solana coinstack boilerplate (#1046)

---
+++ node/coinstacks/solana/api/src/models.ts
@@ -0,0 +1,39 @@
+import { BaseAccount, BaseTx, BaseTxHistory } from '../../../common/api/src' // unable to import models from a module with tsoa
+
+/**
+ * Contains info about a transaction
+ */
+export interface Tx extends BaseTx {}
+
+/**
+ * Contains info about transaction history
+ */
+export type TxHistory = BaseTxHistory<Tx>
+
+/**
+ * Contains info about an address associated with an extended public key
+ */
+export interface Address {
+  balance: string
+  pubkey: string
+}
+
+/**
+ * Contains info about an address or extended public key account
+ */
+export interface Account extends BaseAccount {}
+
+/**
+ * Extended coin specific functionality
+ */
+export interface API {
+  /**
+   * Get transaction details
+   *
+   * @param {string} txid transaction hash
+   *
+   * @returns {Promise<Tx>} transaction payload
+   */
+  // @Get('tx/{txid}')
+  getTransaction(txid: string): Promise<Tx>
+}
