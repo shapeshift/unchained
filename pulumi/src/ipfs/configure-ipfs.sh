f4fc3ad8b1540fa22e7e3d235608c0fb17d7821a
kevin <35275952+kaladinlight@users.noreply.github.com>
2024-12-09 12:12:55 -0700

    fix: set ipfs ownership to correct user (#1082)

--- pulumi/src/ipfs/configure-ipfs.sh
+++ pulumi/src/ipfs/configure-ipfs.sh
@@ -4,24 +4,26 @@ set -ex
 
 user=ipfs
 
-mkdir -p /data/ipfs && chown -R "$user" /data/ipfs
+mkdir -p /data/ipfs
 
 if [ ! -f /data/ipfs/config ]; then
   ipfs init --profile=server
-else
-  if [ -f /data/ipfs/repo.lock ]; then
-    rm /data/ipfs/repo.lock
-  fi
-
-  ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
-  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-  ipfs config Swarm.ConnMgr.GracePeriod 20s
-  ipfs config --json Swarm.ConnMgr.HighWater 10000
-  ipfs config --json Swarm.ConnMgr.LowWater 2500
-  ipfs config --json Internal.Bitswap.EngineBlockstoreWorkerCount 2500
-  ipfs config --json Internal.Bitswap.EngineTaskWorkerCount 500
-  ipfs config --json Internal.Bitswap.MaxOutstandingBytesPerPeer 1048576
-  ipfs config --json Internal.Bitswap.TaskWorkerCount 500
-  ipfs config --json Datastore.BloomFilterSize 1048576
-  ipfs config Datastore.StorageMax 100GB
 fi
+
+if [ -f /data/ipfs/repo.lock ]; then
+  rm /data/ipfs/repo.lock
+fi
+
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+ipfs config Swarm.ConnMgr.GracePeriod 20s
+ipfs config --json Swarm.ConnMgr.HighWater 10000
+ipfs config --json Swarm.ConnMgr.LowWater 2500
+ipfs config --json Internal.Bitswap.EngineBlockstoreWorkerCount 2500
+ipfs config --json Internal.Bitswap.EngineTaskWorkerCount 500
+ipfs config --json Internal.Bitswap.MaxOutstandingBytesPerPeer 1048576
+ipfs config --json Internal.Bitswap.TaskWorkerCount 500
+ipfs config --json Datastore.BloomFilterSize 1048576
+ipfs config Datastore.StorageMax 100GB
+
+chown -R "user" /data/ipfs
