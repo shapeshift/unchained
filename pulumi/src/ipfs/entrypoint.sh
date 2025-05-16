cd1713079afc653e75b083962e81362edc7495f8
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-05-16 14:22:29 -0600

    fix: ipfs cluster config (#1120)

--- pulumi/src/ipfs/entrypoint.sh
+++ pulumi/src/ipfs/entrypoint.sh
@@ -15,5 +15,5 @@ grep -q ".*ipfs-0.*" /proc/sys/kernel/hostname
 if [ $? -eq 0 ]; then
   exec ipfs-cluster-service daemon --upgrade
 else
-  exec ipfs-cluster-service daemon --upgrade --bootstrap /dns4/${SVC_NAME}-0/tcp/9096/ipfs/${CLUSTER_ID} --leave
+  exec ipfs-cluster-service daemon --upgrade --bootstrap /dns4/ipfs-0.${SVC_NAME}/tcp/9096/p2p/${BOOTSTRAP_PEER_ID} --leave
 fi
