0f985a162cf6fd6fcfc977eec53504b7cfbb469d
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-07-28 12:17:25 -0600

    fix: support both nownodes and liquify config

--- node/coinstacks/arbitrum/api/src/controller.ts
+++ node/coinstacks/arbitrum/api/src/controller.ts
@@ -22,17 +22,24 @@ if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
 if (!NETWORK) throw new Error('NETWORK env var not set')
 if (!RPC_URL) throw new Error('RPC_URL env var not set')
 
+const IS_LIQUIFY = RPC_URL.toLowerCase().includes('liquify') && INDEXER_URL.toLowerCase().includes('liquify')
+const IS_NOWNODES = RPC_URL.toLowerCase().includes('nownodes') && INDEXER_URL.toLowerCase().includes('nownodes')
+
 export const logger = new Logger({
   namespace: ['unchained', 'coinstacks', 'arbitrum', 'api'],
   level: process.env.LOG_LEVEL,
 })
 
-const httpURL = INDEXER_API_KEY ? `${INDEXER_URL}/api=${INDEXER_API_KEY}` : INDEXER_URL
-const wsURL = INDEXER_API_KEY ? `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}` : INDEXER_WS_URL
-const rpcUrl = RPC_API_KEY ? `${RPC_URL}/api=${RPC_API_KEY}` : RPC_URL
+const httpURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_URL}/api=${INDEXER_API_KEY}` : INDEXER_URL
+const wsURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}` : INDEXER_WS_URL
+const rpcUrl = RPC_API_KEY && IS_LIQUIFY ? `${RPC_URL}/api=${RPC_API_KEY}` : RPC_URL
 
-const blockbook = new Blockbook({ httpURL, wsURL, logger })
-const provider = new ethers.providers.JsonRpcProvider({ url: rpcUrl })
+const apiKey = INDEXER_API_KEY && IS_NOWNODES ? INDEXER_API_KEY : undefined
+const headers = RPC_API_KEY && IS_NOWNODES ? { 'api-key': RPC_API_KEY } : undefined
+const rpcApiKey = RPC_API_KEY && IS_NOWNODES ? RPC_API_KEY : undefined
+
+const blockbook = new Blockbook({ httpURL, wsURL, logger, apiKey })
+const provider = new ethers.providers.JsonRpcProvider({ url: rpcUrl, headers })
 
 export const gasOracle = new GasOracle({ logger, provider, coinstack: 'arbitrum' })
 
@@ -43,6 +50,7 @@ export const service = new Service({
   provider,
   logger,
   rpcUrl,
+  rpcApiKey,
 })
 
 // assign service to be used for all instances of EVM
