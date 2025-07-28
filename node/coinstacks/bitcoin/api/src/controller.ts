cc0e1a9abc311faee006a2178bc062d1a2af2169
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-07-28 13:10:11 -0600

    fix: support both nownodes and liquify config (#1131)

--- node/coinstacks/bitcoin/api/src/controller.ts
+++ node/coinstacks/bitcoin/api/src/controller.ts
@@ -14,16 +14,22 @@ if (!INDEXER_URL) throw new Error('INDEXER_URL env var not set')
 if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
 if (!RPC_URL) throw new Error('RPC_URL env var not set')
 
+const IS_LIQUIFY = RPC_URL.toLowerCase().includes('liquify') && INDEXER_URL.toLowerCase().includes('liquify')
+const IS_NOWNODES = RPC_URL.toLowerCase().includes('nownodes') && INDEXER_URL.toLowerCase().includes('nownodes')
+
 export const logger = new Logger({
   namespace: ['unchained', 'coinstacks', 'bitcoin', 'api'],
   level: process.env.LOG_LEVEL,
 })
 
-const httpURL = INDEXER_API_KEY ? `${INDEXER_URL}/api=${INDEXER_API_KEY}` : INDEXER_URL
-const wsURL = INDEXER_API_KEY ? `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}` : INDEXER_WS_URL
-const rpcUrl = RPC_API_KEY ? `${RPC_URL}/api=${RPC_API_KEY}` : RPC_URL
+const httpURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_URL}/api=${INDEXER_API_KEY}` : INDEXER_URL
+const wsURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}` : INDEXER_WS_URL
+const rpcUrl = RPC_API_KEY && IS_LIQUIFY ? `${RPC_URL}/api=${RPC_API_KEY}` : RPC_URL
 
-const blockbook = new Blockbook({ httpURL, wsURL, logger })
+const apiKey = INDEXER_API_KEY && IS_NOWNODES ? INDEXER_API_KEY : undefined
+const rpcApiKey = RPC_API_KEY && IS_NOWNODES ? RPC_API_KEY : undefined
+
+const blockbook = new Blockbook({ httpURL, wsURL, logger, apiKey })
 
 const isXpub = (pubkey: string): boolean => {
   return pubkey.startsWith('xpub') || pubkey.startsWith('ypub') || pubkey.startsWith('zpub')
@@ -40,6 +46,7 @@ export const service = new Service({
   rpcUrl,
   isXpub,
   addressFormatter: formatAddress,
+  rpcApiKey,
 })
 
 // assign service to be used for all instances of UTXO
