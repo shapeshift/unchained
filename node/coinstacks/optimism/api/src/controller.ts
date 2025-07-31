704cb63ccf634aa3490948fb6e1c9182235b0b44
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-07-31 11:10:36 -0600

    feat: optimism on liquify (develop) (#1136)

--- node/coinstacks/optimism/api/src/controller.ts
+++ node/coinstacks/optimism/api/src/controller.ts
@@ -25,6 +25,9 @@ if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
 if (!NETWORK) throw new Error('NETWORK env var not set')
 if (!RPC_URL) throw new Error('RPC_URL env var not set')
 
+const IS_LIQUIFY = RPC_URL.toLowerCase().includes('liquify') && INDEXER_URL.toLowerCase().includes('liquify')
+const IS_NOWNODES = RPC_URL.toLowerCase().includes('nownodes') && INDEXER_URL.toLowerCase().includes('nownodes')
+
 export const logger = new Logger({
   namespace: ['unchained', 'coinstacks', 'optimism', 'api'],
   level: process.env.LOG_LEVEL,
@@ -33,9 +36,16 @@ export const logger = new Logger({
 const CHAIN_ID: Record<string, number> = { mainnet: 10 }
 const GAS_PRICE_ORACLE_ADDRESS = '0x420000000000000000000000000000000000000F'
 
-const blockbook = new Blockbook({ httpURL: INDEXER_URL, wsURL: INDEXER_WS_URL, apiKey: INDEXER_API_KEY, logger })
-const headers = RPC_API_KEY ? { 'api-key': RPC_API_KEY } : undefined
-const provider = new ethers.providers.JsonRpcProvider({ url: RPC_URL, headers })
+const httpURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_URL}/api=${INDEXER_API_KEY}` : INDEXER_URL
+const wsURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}` : INDEXER_WS_URL
+const rpcUrl = RPC_API_KEY && IS_LIQUIFY ? `${RPC_URL}/api=${RPC_API_KEY}` : RPC_URL
+
+const apiKey = INDEXER_API_KEY && IS_NOWNODES ? INDEXER_API_KEY : undefined
+const headers = RPC_API_KEY && IS_NOWNODES ? { 'api-key': RPC_API_KEY } : undefined
+const rpcApiKey = RPC_API_KEY && IS_NOWNODES ? RPC_API_KEY : undefined
+
+const blockbook = new Blockbook({ httpURL, wsURL, logger, apiKey })
+const provider = new ethers.providers.JsonRpcProvider({ url: rpcUrl, headers })
 export const gasOracle = new GasOracle({ logger, provider, coinstack: 'optimism' })
 
 export const service = new Service({
@@ -44,8 +54,8 @@ export const service = new Service({
   explorerApiUrl: new URL(`https://api.etherscan.io/v2/api?chainid=10&apikey=${ETHERSCAN_API_KEY}`),
   provider,
   logger,
-  rpcUrl: RPC_URL,
-  rpcApiKey: RPC_API_KEY,
+  rpcUrl,
+  rpcApiKey,
 })
 
 // assign service to be used for all instances of EVM
