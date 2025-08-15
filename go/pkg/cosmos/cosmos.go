23f7c90fa1046efb48be6afa8a2d0c523e032dc2
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-08-15 11:27:46 -0600

    chore: remove grpc cruft

--- go/pkg/cosmos/cosmos.go
+++ go/pkg/cosmos/cosmos.go
@@ -1,4 +1,4 @@
-// Package cosmos provides a client wrapper with support for both http and grpc.
+// Package cosmos provides an http client wrapper.
 package cosmos
 
 import (
@@ -14,22 +14,16 @@ import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	stdtypes "github.com/cosmos/cosmos-sdk/std"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	authztypes "github.com/cosmos/cosmos-sdk/x/authz"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	govv1types "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	govv1beta1types "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/go-resty/resty/v2"
 	"github.com/pkg/errors"
 	"github.com/shapeshift/unchained/internal/log"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/metadata"
 )
 
 var logger = log.WithoutFields()
@@ -82,7 +76,6 @@ type Config struct {
 	Denom             string
 	NativeFee         int
 	Encoding          *params.EncodingConfig
-	GRPCURL           string
 	LCDURL            string
 	RPCURL            string
 	WSURL             string
@@ -130,66 +123,6 @@ func NewHTTPClient(conf Config) (*HTTPClient, error) {
 
 func (c *HTTPClient) GetEncoding() *params.EncodingConfig {
 	return c.encoding
-}
-
-// GRPCClient allows communicating over grpc
-type GRPCClient struct {
-	ctx      context.Context
-	encoding *params.EncodingConfig
-	grpcConn *grpc.ClientConn
-
-	auth         authtypes.QueryClient
-	bank         banktypes.QueryClient
-	distribution distributiontypes.QueryClient
-	mint         minttypes.QueryClient
-	staking      stakingtypes.QueryClient
-	tx           txtypes.ServiceClient
-}
-
-// NewGRPCClient configures and creates a GRPClient
-func NewGRPCClient(conf Config) (*GRPCClient, error) {
-	sdk.GetConfig().SetBech32PrefixForAccount(conf.Bech32AddrPrefix, conf.Bech32PkPrefix)
-
-	grpcURL, err := url.Parse(conf.GRPCURL)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to parse GRPCURL: %s", conf.GRPCURL)
-	}
-
-	md := metadata.Pairs()
-	ctx := metadata.NewOutgoingContext(context.Background(), md)
-
-	// The Cosmos SDK doesn't support any transport security mechanism.
-	grpcConn, err := grpc.NewClient(grpcURL.String(), grpc.WithTransportCredentials(credentials.NewTLS(nil)))
-
-	if err != nil {
-		return nil, errors.Wrapf(err, "unable to connect to: %s", grpcURL)
-	}
-
-	auth := authtypes.NewQueryClient(grpcConn)
-	bank := banktypes.NewQueryClient(grpcConn)
-	distribution := distributiontypes.NewQueryClient(grpcConn)
-	mint := minttypes.NewQueryClient(grpcConn)
-	staking := stakingtypes.NewQueryClient(grpcConn)
-	tx := txtypes.NewServiceClient(grpcConn)
-
-	c := &GRPCClient{
-		ctx:          ctx,
-		grpcConn:     grpcConn,
-		encoding:     conf.Encoding,
-		auth:         auth,
-		bank:         bank,
-		distribution: distribution,
-		mint:         mint,
-		staking:      staking,
-		tx:           tx,
-	}
-
-	return c, nil
-}
-
-// Close any GRPCClient connections
-func (c *GRPCClient) Close() {
-	c.grpcConn.Close()
 }
 
 // NewEncoding registers all base protobuf types by default as well as any custom types passed in
