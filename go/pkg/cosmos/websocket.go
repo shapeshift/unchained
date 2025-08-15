bdb7b35fcb838a6072622cb401a2e339ca3d5c5c
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-08-15 11:42:06 -0600

    feat: nownodes cosmos (#994)

--- go/pkg/cosmos/websocket.go
+++ go/pkg/cosmos/websocket.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+	"strings"
 	"sync"
 	"time"
 
@@ -49,7 +50,13 @@ func NewWebsocketClient(conf Config, blockService *BlockService, errChan chan<- 
 		return nil, errors.Wrapf(err, "failed to parse WSURL: %s", conf.WSURL)
 	}
 
-	client, err := cometbft.NewWS(wsURL.String(), "/websocket")
+	endpoint := "/websocket"
+	if conf.APIKEY != "" {
+		parts := strings.Split(conf.APIKEY, ":")
+		endpoint = fmt.Sprintf("/%s/websocket", parts[1])
+	}
+
+	client, err := cometbft.NewWS(wsURL.String(), endpoint)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to create websocket client")
 	}
