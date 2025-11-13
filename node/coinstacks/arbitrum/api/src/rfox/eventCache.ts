3622b089367dc39bc177f11099c93ea83f83ee9c
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-11-13 16:33:34 -0700

    event cache with infura backend

--- node/coinstacks/arbitrum/api/src/rfox/eventCache.ts
+++ node/coinstacks/arbitrum/api/src/rfox/eventCache.ts
@@ -1,5 +1,6 @@
 import { PublicClient, Address, GetContractEventsReturnType } from 'viem'
 import { RFOX_ABI } from './abi'
+import { Logger } from '@shapeshiftoss/logger'
 
 const FOX_PROXY_CONTRACT: Address = '0xaC2a4fD70BCD8Bab0662960455c363735f0e2b56'
 const UNI_V2_ETH_FOX_PROXY_CONTRACT: Address = '0x83B51B7605d2E277E03A7D6451B1efc0e5253A2F'
@@ -12,96 +13,93 @@ interface Events {
   lastIndexedBlock: bigint
 }
 
+export interface EventCacheArgs {
+  client: PublicClient
+  infuraClient: PublicClient
+  logger: Logger
+}
+
 export class EventCache {
   private client: PublicClient
+  private infuraClient: PublicClient
+  private logger: Logger
 
   private cache: Map<Address, Events> = new Map([
     [FOX_PROXY_CONTRACT, { eventsByAddress: new Map(), lastIndexedBlock: 222913582n }],
     [UNI_V2_ETH_FOX_PROXY_CONTRACT, { eventsByAddress: new Map(), lastIndexedBlock: 291163572n }],
   ])
 
-  constructor(client: PublicClient) {
-    this.client = client
+  constructor(args: EventCacheArgs) {
+    this.client = args.client
+    this.infuraClient = args.infuraClient
+    this.logger = args.logger.child({ namespace: ['eventCache'] })
   }
 
   async initialize() {
-    const indexEvents = async () => {
-      await Promise.all(
-        Array.from(this.cache.keys()).map(async (contractAddress) => {
-          await this.indexContractEvents(contractAddress)
-        })
-      )
-    }
-
     try {
-      console.log('Starting initial cache update')
-      await indexEvents()
-      console.log('Initial update complete')
+      this.logger.info('Event cache initalizing')
+      for (const contractAddress of Array.from(this.cache.keys())) {
+        await this.indexContractEvents(contractAddress)
+      }
+      this.logger.info('Event cache initialized')
     } catch (err) {
-      console.log('Error during initial cache update:', err)
+      this.logger.error(err, 'Failed to initialize event cache')
     }
 
     setInterval(
       async () => {
         try {
-          console.log('Starting periodic cache update...')
-          await indexEvents()
-          console.log('Periodic update complete')
+          this.logger.info('Event cache updating')
+          for (const contractAddress of Array.from(this.cache.keys())) {
+            await this.indexContractEvents(contractAddress)
+          }
+          this.logger.info('Event cache updated')
         } catch (err) {
-          console.error('Error during periodic update:', err)
+          this.logger.error(err, 'Failed to update event cache')
         }
       },
-      1000 * 60 * 1
+      1000 * 60 * 15 // 15 minute update frequency
     )
   }
 
   private async indexContractEvents(contractAddress: Address) {
-    try {
-      const startBlock = this.cache.get(contractAddress)!.lastIndexedBlock
-      const endBlock = await this.client.getBlockNumber()
+    const startBlock = this.cache.get(contractAddress)!.lastIndexedBlock
+    const endBlock = await this.client.getBlockNumber()
 
-      console.log(`Indexing ${contractAddress} from block ${startBlock} to ${endBlock}`)
+    const events = await this.fetchEvents(contractAddress, startBlock, endBlock)
 
-      const events = await this.fetchEvents(contractAddress, startBlock, endBlock)
+    const eventsByAddress = new Map(this.cache.get(contractAddress)!.eventsByAddress)
+    events.forEach((event) => {
+      const accountAddress = event.args.account!.toLowerCase()
+      if (!eventsByAddress.has(accountAddress)) eventsByAddress.set(accountAddress, [])
+      eventsByAddress.get(accountAddress)!.push(event)
+    })
 
-      const eventsByAddress = new Map(this.cache.get(contractAddress)!.eventsByAddress)
-      events.forEach((event) => {
-        const accountAddress = event.args.account!.toLowerCase()
-        if (!eventsByAddress.has(accountAddress)) eventsByAddress.set(accountAddress, [])
-        eventsByAddress.get(accountAddress)!.push(event)
-      })
+    this.cache.set(contractAddress, {
+      eventsByAddress: eventsByAddress,
+      lastIndexedBlock: endBlock,
+    })
 
-      this.cache.set(contractAddress, {
-        eventsByAddress: eventsByAddress,
-        lastIndexedBlock: endBlock,
-      })
-
-      console.log(`✅ Indexed ${events.length} events for ${eventsByAddress.size} accounts`)
-    } catch (err) {
-      console.log(`Failed to index ${contractAddress}`)
-      throw err
-    }
+    this.logger.info(`Indexed ${events.length} events (${eventsByAddress.size} accounts) for ${contractAddress}`)
   }
 
   private async fetchEvents(contractAddress: Address, startBlock: bigint, endBlock: bigint): Promise<Array<Event>> {
     try {
       const events: Array<Event> = []
 
-      console.log(`Fetching events for ${contractAddress} from ${startBlock} to ${endBlock}`)
+      this.logger.info(`Fetching events for ${contractAddress} from ${startBlock} to ${endBlock}`)
 
-      const [stakeEvents, unstakeEvents] = await Promise.all(
-        (['Stake', 'Unstake'] as const).map((eventName) => {
-          return this.client.getContractEvents({
-            address: contractAddress,
-            eventName,
-            abi: RFOX_ABI,
-            fromBlock: startBlock,
-            toBlock: endBlock,
-          })
+      for (const eventName of ['Stake', 'Unstake'] as const) {
+        const contractEvents = await this.infuraClient.getContractEvents({
+          address: contractAddress,
+          eventName,
+          abi: RFOX_ABI,
+          fromBlock: startBlock,
+          toBlock: endBlock,
         })
-      )
 
-      events.push(...stakeEvents, ...unstakeEvents)
+        events.push(...contractEvents)
+      }
 
       events.sort((a, b) => {
         if (a.blockNumber !== b.blockNumber) return Number(a.blockNumber - b.blockNumber)
@@ -110,7 +108,7 @@ export class EventCache {
 
       return events
     } catch (err) {
-      console.log(`Failed to fetch events for ${contractAddress} from ${startBlock} to ${endBlock}`)
+      this.logger.error(`Failed to fetch events for ${contractAddress} from ${startBlock} to ${endBlock}`)
       throw err
     }
   }
