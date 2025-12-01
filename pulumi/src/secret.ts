f254c9e2c961a018a2655fa49fe2cdf05adf2e8e
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-12-01 11:23:20 -0700

    chore: public unchained pulumi package

---
+++ pulumi/src/secret.ts
@@ -0,0 +1,36 @@
+import * as k8s from '@pulumi/kubernetes'
+import { CustomResourceOptions } from '@pulumi/pulumi'
+import { parse } from 'dotenv'
+
+export interface SecretArgs {
+  name: string
+  env: Buffer
+  namespace: string
+}
+
+export const createSecret = (args: SecretArgs, opts: CustomResourceOptions) => {
+  const { name, env, namespace } = args
+  const secretData = getSecretData(env)
+
+  new k8s.core.v1.Secret(name, { metadata: { name, namespace }, stringData: secretData }, opts)
+}
+
+const getSecretData = (sampleEnv: Buffer): Record<string, string> => {
+  const missingKeys: Array<string> = []
+  const stringData = Object.keys(parse(sampleEnv)).reduce((prev, key) => {
+    const value = process.env[key]
+
+    if (value === undefined) {
+      missingKeys.push(key)
+      return prev
+    }
+
+    return { ...prev, [key]: value }
+  }, {})
+
+  if (missingKeys.length) {
+    throw new Error(`Missing the following required environment variables: ${missingKeys.join(', ')}`)
+  }
+
+  return stringData
+}
