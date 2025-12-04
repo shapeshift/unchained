a71d7c16f3c3164d0dabcb05f7566266a7938880
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-12-04 13:44:28 -0700

    feat: break out utxo getNetworkFees (#1225)

--- node/coinstacks/litecoin/api/src/controller.ts
+++ node/coinstacks/litecoin/api/src/controller.ts
@@ -1,5 +1,7 @@
-import { bech32 } from 'bech32'
 import { Blockbook } from '@shapeshiftoss/blockbook'
+import { bech32 } from 'bech32'
+import { Get, Response, Route, Tags } from 'tsoa'
+import { BadRequestError, BaseAPI, InternalServerError, utxo } from '../../../common/api/src'
 import { Service } from '../../../common/api/src/utxo/service'
 import { UTXO } from '../../../common/api/src/utxo/controller'
 import { Logger } from '@shapeshiftoss/logger'
@@ -12,24 +14,20 @@ const RPC_API_KEY = process.env.RPC_API_KEY
 
 if (!INDEXER_URL) throw new Error('INDEXER_URL env var not set')
 if (!INDEXER_WS_URL) throw new Error('INDEXER_WS_URL env var not set')
+if (!INDEXER_API_KEY) throw new Error('INDEXER_API_KEY env var not set')
 if (!RPC_URL) throw new Error('RPC_URL env var not set')
-
-const IS_LIQUIFY = RPC_URL.toLowerCase().includes('liquify') && INDEXER_URL.toLowerCase().includes('liquify')
-const IS_NOWNODES = RPC_URL.toLowerCase().includes('nownodes') && INDEXER_URL.toLowerCase().includes('nownodes')
+if (!RPC_API_KEY) throw new Error('RPC_API_KEY env var not set')
 
 export const logger = new Logger({
   namespace: ['unchained', 'coinstacks', 'litecoin', 'api'],
   level: process.env.LOG_LEVEL,
 })
 
-const httpURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_URL}/api=${INDEXER_API_KEY}` : INDEXER_URL
-const wsURL = INDEXER_API_KEY && IS_LIQUIFY ? `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}` : INDEXER_WS_URL
-const rpcUrl = RPC_API_KEY && IS_LIQUIFY ? `${RPC_URL}/api=${RPC_API_KEY}` : RPC_URL
+const httpURL = `${INDEXER_URL}/api=${INDEXER_API_KEY}`
+const wsURL = `${INDEXER_WS_URL}/api=${INDEXER_API_KEY}`
+const rpcUrl = `${RPC_URL}/api=${RPC_API_KEY}`
 
-const apiKey = INDEXER_API_KEY && IS_NOWNODES ? INDEXER_API_KEY : undefined
-const rpcApiKey = RPC_API_KEY && IS_NOWNODES ? RPC_API_KEY : undefined
-
-const blockbook = new Blockbook({ httpURL, wsURL, logger, apiKey })
+const blockbook = new Blockbook({ httpURL, wsURL, logger })
 
 const isXpub = (pubkey: string): boolean => {
   return pubkey.startsWith('Ltub') || pubkey.startsWith('Mtub') || pubkey.startsWith('zpub')
@@ -44,10 +42,25 @@ export const formatAddress = (address: string): string => {
 export const service = new Service({
   blockbook,
   rpcUrl,
-  rpcApiKey,
   isXpub,
   addressFormatter: formatAddress,
 })
 
 // assign service to be used for all instances of UTXO
 UTXO.service = service
+
+@Route('api/v1')
+@Tags('v1')
+export class Litecoin extends UTXO implements BaseAPI, utxo.API {
+  /**
+   * Get current recommended network fees to use in a transaction
+   *
+   * @returns {Promise<NetworkFees>} current network fees
+   */
+  @Response<BadRequestError>(400, 'Bad Request')
+  @Response<InternalServerError>(500, 'Internal Server Error')
+  @Get('/fees')
+  async getNetworkFees(): Promise<utxo.NetworkFees> {
+    return UTXO.service.getNetworkFees()
+  }
+}
