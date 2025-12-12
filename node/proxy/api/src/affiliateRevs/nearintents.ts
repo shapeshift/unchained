0218f233ac6f7d5552e0aa0acf5cec0c5f39f56f
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-12-12 14:21:05 -0700

    nearintents starting point

---
+++ node/proxy/api/src/affiliateRevs/nearintents.ts
@@ -0,0 +1,89 @@
+import axios from 'axios'
+import { AffiliateRevenue } from '.'
+
+const API_KEY = ''
+
+type Transaction = {
+  originAsset: string
+  destinationAsset: string
+  depositAddress: string
+  recipient: string
+  status: string
+  createdAt: string
+  createdAtTimestamp: number
+  intentHashes: string
+  referral: string
+  amountInFormatted: string
+  amountOutFormatted: string
+  appFees: Array<{
+    fee: number
+    recipient: string
+  }>
+  nearTxHashes: string[]
+  originChainTxHashes: string[]
+  destinationChainTxHashes: string[]
+  amountIn: string
+  amountInUsd: string
+  amountOut: string
+  amountOutUsd: string
+  refundTo: string
+}
+
+type TransactionsResponse = {
+  data: Transaction[]
+  totalPages: number
+  page: number
+  perPage: number
+  total: number
+  nextPage?: number
+  prevPage?: number
+}
+
+export const getAffiliateRevenue = async (
+  startTimestamp: number,
+  endTimestamp: number
+): Promise<Array<AffiliateRevenue>> => {
+  const revenues: Array<AffiliateRevenue> = []
+
+  let page: number | undefined = 1
+  while (page) {
+    const { data } = (await axios.get<TransactionsResponse>(
+      'https://explorer.near-intents.org/api/v0/transactions-pages',
+      {
+        params: {
+          referral: 'shapeshift',
+          page,
+          perPage: 100,
+          statuses: 'SUCCESS',
+          startTimestampUnix: startTimestamp,
+          endTimestampUnix: endTimestamp,
+        },
+        headers: { Authorization: `Bearer ${API_KEY}` },
+      }
+    )) as { data: TransactionsResponse }
+
+    for (const transaction of data.data) {
+      for (const appFee of transaction.appFees) {
+        // TODO: Figure out parsing of originAsset/destinationAsset
+        const chainId = ''
+        const assetId = `${chainId}/`
+
+        revenues.push({
+          chainId,
+          assetId,
+          service: 'nearintents',
+          txHash: '', // TODO: determine if origin or destination chain tx hash is directly tied to the fee or which makes the most sense to use
+          timestamp: transaction.createdAtTimestamp,
+          amount: String((parseFloat(transaction.amountIn) * appFee.fee) / 10000),
+          amountUsd: String((parseFloat(transaction.amountInUsd) * appFee.fee) / 10000),
+        })
+      }
+    }
+
+    page = data.nextPage
+  }
+
+  console.log(`Near Intents: Found ${revenues.length} affiliate fees`)
+
+  return revenues
+}
