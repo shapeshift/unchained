8b8f7a0a4c9d51f829e3b8f058cef634e6501eeb
kevin <35275952+kaladinlight@users.noreply.github.com>
2026-03-31 13:54:59 -0600

    feat: nownodes cosmos support (#1270)

--- go/pkg/cosmos/websocket.go
+++ go/pkg/cosmos/websocket.go
@@ -41,29 +41,28 @@ type WSClient struct {
 }
 
 func NewWebsocketClient(conf cosmossdk.Config, blockService *cosmossdk.BlockService, errChan chan<- error) (*WSClient, error) {
-	isLiquify := strings.Contains(conf.WSURL, "liquify")
-	isNownodes := strings.Contains(conf.WSURL, "nownodes")
-
 	wsURL, err := url.Parse(conf.WSURL)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to parse WSURL: %s", conf.WSURL)
 	}
 
 	endpoint := "/websocket"
-	if conf.RPCAPIKEY != "" {
-		if isNownodes {
-			parts := strings.Split(conf.RPCAPIKEY, ":")
-			endpoint = fmt.Sprintf("/%s/websocket", parts[1])
-		}
-
-		if isLiquify {
-			endpoint = fmt.Sprintf("/api=%s/websocket", conf.RPCAPIKEY)
-		}
+	if conf.WSAPIKEY != "" {
+		endpoint = "/wss"
 	}
 
 	client, err := cometbft.NewWS(wsURL.String(), endpoint)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to create websocket client")
+	}
+
+	if conf.WSAPIKEY != "" {
+		username, password, ok := strings.Cut(conf.WSAPIKEY, ":")
+		if !ok {
+			return nil, errors.New("invalid WSAPIKEY format: expected 'username:password'")
+		}
+		client.Username = username
+		client.Password = password
 	}
 
 	// use default dialer
